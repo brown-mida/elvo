a6e879ddfbe129746f8431c14abf1a72ef9d856c
luke-zhu <luke_zhu@brown.edu>
2018-04-04 19:22:47 -0400

    Updated preprocessing code.

--- preprocessors/preprocessor.py
+++ preprocessors/preprocessor.py
@@ -1,37 +1,39 @@
 import logging
 import os
-import time
 
 import numpy as np
 import pandas as pd
-import scipy.ndimage
-from keras.callbacks import TensorBoard, ModelCheckpoint
-from keras.optimizers import Adam
+from google.cloud import storage
 
 import preprocessors.parsers as parsers
 import preprocessors.transforms as transforms
 
 
-def preprocess(ct_dir, roi_dir, output_dir):
+def preprocess(bucket_name, roi_dir, output_dir):
     """Loads the data from the input directory and saves
     normalized, zero-centered, 200 x 200 x 200 3D renderings
     in output_dir.
     """
-    parsers.unzip_scans(ct_dir)
-    logging.debug('Unzipped data in {}'.format(ct_dir))
-    patient_ids = parsers.load_patient_infos(ct_dir)
-    logging.debug('Loaded patient ids in {}'.format(ct_dir))
+    # parsers.unzip_scans(bucket_name)
+    # logging.debug('Unzipped data in {}'.format(bucket_name))
+    # patient_ids = parsers.load_patient_infos(bucket_name)
+    # logging.debug('Loaded patient ids in {}'.format(bucket_name))
+    df = pd.read_excel('/home/shared/data/elvos_meta_drop1.xls')
 
     os.makedirs(output_dir)
-    for id_, path in patient_ids.items():
+    for id_ in df.items():
         try:
+            filename = id_ + '.zip'
+            _download_blob(bucket_name, filename, filename)
+            print('Done')
+            return
             slices = parsers.load_scan(path)
             logging.debug('Loaded slices for patient {}'.format(id_))
             scan = _preprocess_scan(slices)
             _save_scan(id_, scan, output_dir)
         except Exception as e:
             # TODO(Luke): Remove after first run
-            logging.error('Failed to load {}'.format(id_))
+            logging.error('Failed to preprocess {}'.format(id_))
             logging.error(e)
 
     # Consider doing this step just before training the model
@@ -40,12 +42,25 @@ def preprocess(ct_dir, roi_dir, output_dir):
     _save_info(patient_ids, roi_dir, output_dir)
 
 
+def _download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+
+    blob.download_to_filename(destination_file_name)
+
+    print('Blob {} downloaded to {}.'.format(
+        source_blob_name,
+        destination_file_name))
+
+
 def _preprocess_scan(slices):
     """Transforms the CT slices into a processed 3D numpy array.
     """
     scan = transforms.get_pixels_hu(slices)
     scan = transforms.standardize_spacing(scan, slices)
-    scan = transforms.crop(scan)
+    # scan = transforms.crop(scan)
     # TODO: Generate an image at this point to verify the preprocessing
     logging.debug(
         'Finished converting to HU, standardizing pixels'
@@ -87,5 +102,3 @@ def _save_info(patient_ids, roi_dir, output_dir):
             pass
 
     info.to_csv('{}/labels.csv'.format(output_dir))
-
-
---
+++ scripts/preprocess.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+
+from preprocessors.preprocessor import preprocess
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Preprocesses the ELVO scans')
+    parser.add_argument(
+        'ct_dir',
+        help='Path to the directory holding anonymized folders of CT scans',
+    )
+    parser.add_argument(
+        'roi_dir',
+        help='Path to the directory holding'
+             ' anonymized folders of ROI annotations',
+    )
+    parser.add_argument(
+        'output_dir',
+        help='Path to write the processed data to',
+    )
+    args = parser.parse_args()
+    preprocess(args.ct_dir, args.roi_dir, args.output_dir)
