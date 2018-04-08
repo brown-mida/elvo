2d668e54d12d976e07203a06fea4ed5b5ae0c5c2
rladbsgh <therladbsgh@gmail.com>
2018-04-08 03:47:38 -0400

    Add Alexnet3D model

---
+++ models/alexnet3d.py
@@ -0,0 +1,85 @@
+"""
+A simple 3D Convolutional network. It has 5 convolutional
+layers and 3 FC layers, much like Alexnet.
+
+Unfortunately, the architecture described in the paper is
+severely lacking in details. Here I assume:
+- That the depth of the image is kept constant throughout
+  the convolutional layers
+- The filter sizes
+- The strides between convolutional layers
+- The pooling size
+
+Based on:
+- https://github.com/ehosseiniasl/3d-convolutional-network
+- https://www.nature.com/articles/s41746-017-0015-z.pdf
+"""
+
+from keras.models import Model
+from keras.layers import Input, BatchNormalization, Dense, Flatten
+from keras.layers.convolutional import Conv3D, MaxPooling3D
+
+
+class AlexNet3DBuilder(object):
+
+    @staticmethod
+    def build(input_shape):
+        """Create a 3D Convolutional Autoencoder model.
+
+        Parameters:
+        - input_shape: Tuple of input shape in the format
+            (conv_dim1, conv_dim2, conv_dim3, channels)
+        - initial_filter: Initial filter size. This will be doubled
+            for each hidden layer as it goes deeper.
+        - num_encoding_layers: Number of encoding convolutional +
+            pooling layers. The number of decoding
+            layers will be the same.
+
+        Returns:
+        - A 3D CAD model that takes a 5D tensor (volumetric images
+        in batch) as input and returns a 5D vector (prediction) as output.
+        """
+
+        if len(input_shape) != 4:
+            raise ValueError("Input shape should be a tuple "
+                             "(conv_dim1, conv_dim2, conv_dim3, channels)")
+
+        input_img = Input(shape=input_shape, name="cad_input")
+
+        # Conv1 (Output 200 x 200 x 24 x 48)
+        x = Conv3D(48, (3, 3, 3), activation='relu', padding='same')(input_img)
+        x = BatchNormalization()(x)
+        x = MaxPooling3D(pool_size=(4, 4, 2), strides=(4, 4, 2))(x)
+
+        # Conv2 (Output 50 x 50 x 24 x 64)
+        x = Conv3D(64, (3, 3, 3), activation='relu', padding='same')(x)
+        x = BatchNormalization()(x)
+        x = MaxPooling3D(pool_size=(4, 4, 2), strides=(4, 4, 2))(x)
+
+        # Conv3 (Output 12 x 12 x 24 x 96)
+        x = Conv3D(96, (3, 3, 3), activation='relu',
+                   padding='same')(x)
+
+        # Conv4 (Output 6 x 6 x 24 x 128)
+        x = Conv3D(128, (3, 3, 3), activation='relu', strides=(2, 2, 1),
+                   padding='same')(x)
+
+        # Conv5 (Output 3 x 3 x 24 x 128)
+        x = Conv3D(256, (3, 3, 3), activation='relu', strides=(2, 2, 1),
+                   padding='same')(x)
+
+        # Pooling and flatten
+        x = MaxPooling3D(pool_size=(2, 2, 2), strides=(2, 2, 2))(x)
+        x = Flatten()(x)
+
+        # Fully connected layers
+        x = Dense(1000, activation='relu', use_bias=True)(x)
+        x = Dense(1000, activation='relu', use_bias=True)(x)
+        x = Dense(2, activation='relu', use_bias=True)(x)
+
+        model = Model(inputs=input_img, outputs=x)
+        return model
+
+
+# m = AlexNet3DBuilder.build((200, 200, 24, 1))
+# m.summary()
--- sandbox2.py
+++ sandbox2.py
@@ -1,25 +1,107 @@
-import logging
-import os
-import time
-
 import numpy as np
-import pandas as pd
-import scipy.ndimage
-from keras.callbacks import TensorBoard, ModelCheckpoint
-from keras.optimizers import Adam
+import tensorflow as tf
 
-from preprocessors import preprocessor, parsers, transforms
-from generators.t_generator import Generator
-from generators.cad_generator import CadGenerator
 
-from models.resnet3d import Resnet3DBuilder
-from models.autoencoder import Cad3dBuilder
+# Accuracy function
+def get_accuracy(predictions, labels):
+  return 100 * tf.reduce_mean(tf.cast(tf.equal(tf.argmax(predictions,1), tf.argmax(labels,1)), tf.float32))
 
-data_loc = '/home/shared/data/data-20180407'
-gen = CadGenerator(data_loc, batch_size=16)
 
-while True:
-    a = gen.generate()
-    a = next(a)
-    print(a)
-    print(np.shape(a))
+# Graph
+batch_size = 1000
+num_labels = 10
+
+
+graph = tf.Graph()
+
+with graph.as_default():
+
+    predict = tf.Variable(False)
+    # Input data.
+    tf_train_dataset = tf.placeholder(tf.float32, shape=(None, 111, 111, 111, 1))
+    tf_train_labels = tf.placeholder(tf.float32, shape=(None, num_labels))
+
+    # Variables.
+    layer1_weights = tf.Variable(tf.truncated_normal(
+      [9, 9, 9, 1, 96], stddev=0.1))
+
+    layer1_biases = tf.Variable(tf.zeros([96]))
+
+    layer2_weights = tf.Variable(tf.truncated_normal(
+      [5, 5, 5, 96, 256], stddev=0.1))
+
+    layer2_biases = tf.Variable(tf.constant(1.0, shape=[256]))
+
+    layer3_weights = tf.Variable(tf.truncated_normal(
+      [3, 3, 3, 256, 384], stddev=0.1))
+    layer3_biases = tf.Variable(tf.constant(1.0, shape=[384]))
+
+    layer4_weights = tf.Variable(tf.truncated_normal(
+      [3, 3, 3, 384, 384], stddev=0.1))
+    layer4_biases = tf.Variable(tf.constant(1.0, shape=[384]))
+
+    layer5_weights = tf.Variable(tf.truncated_normal(
+      [3, 3, 3, 384, 256], stddev=0.1))
+    layer5_biases = tf.Variable(tf.constant(1.0, shape=[256]))
+
+    layer6_weights = tf.Variable(tf.truncated_normal(
+      [49*49*256, 4096], stddev=0.1))
+    layer6_biases = tf.Variable(tf.constant(1.0, shape=[4096]))
+
+    layer7_weights = tf.Variable(tf.truncated_normal(
+      [4096, 4096], stddev=0.1))
+    layer7_biases = tf.Variable(tf.constant(1.0, shape=[4096]))
+
+    layer8_weights = tf.Variable(tf.truncated_normal(
+      [4096, num_labels], stddev=0.1))
+    layer8_biases = tf.Variable(tf.constant(1.0, shape=[num_labels]))
+
+
+    #MODEL
+    def model(data):
+        # Conv1
+
+        print(data)
+        conv1 = tf.nn.conv3d(data, layer1_weights, [1, 4, 4, 4, 1], padding='SAME')
+        print(conv1)
+        hidden1 = tf.nn.relu(conv1 + layer1_biases)
+
+        #Pool1
+        pool1 = tf.nn.max_pool3d(hidden1, ksize=[1, 3, 3, 3, 1], strides=[1, 2, 2, 2, 1], padding='SAME')
+        print(pool1)
+
+        # Conv2
+        conv2 = tf.nn.conv3d(pool1, layer2_weights, [1, 1, 1, 1, 1],padding='SAME')
+        hidden2 = tf.nn.relu(conv2 + layer2_biases)
+
+        # Conv3
+        conv3 = tf.nn.conv3d(hidden2, layer3_weights, [1, 1, 1, 1, 1],padding='SAME')
+
+        # Conv4
+        conv4 = tf.nn.conv3d(conv3, layer4_weights, [1, 1, 1, 1, 1], padding='SAME')
+
+        # Conv5
+        conv5 = tf.nn.conv3d(conv4, layer5_weights, [1, 1, 1, 1, 1], padding='SAME')
+
+        #Pool2
+        pool2 = tf.nn.max_pool3d(conv5, ksize=[1, 3, 3, 3, 1], strides=[1, 2, 2, 2, 1], padding='SAME')
+
+        normalize3_flat = tf.reshape(pool2, [-1, 49*49*256])
+
+        #FC1
+        fc1 = tf.tanh(tf.add(tf.matmul(normalize3_flat, layer6_weights), layer6_biases))
+        dropout1 = tf.nn.dropout(fc1, 0.5)
+
+        #FC2
+        fc2 = tf.tanh(tf.add(tf.matmul(dropout1, layer7_weights), layer7_biases))
+        dropout2 = tf.nn.dropout(fc2, 0.5)
+
+        #FC3
+        res = tf.nn.softmax(tf.add(tf.matmul(dropout2, layer8_weights), layer8_biases))
+        return res
+
+
+     # Training computation
+    local_res = model(tf_train_dataset)
+    print ('Graph was built')
+
