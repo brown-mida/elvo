3f56ca319a4328a742c973578615e6e458c6e325
rladbsgh <therladbsgh@gmail.com>
2018-04-08 20:33:36 -0400

    Add alexnet train method to main

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ from keras.callbacks import TensorBoard, ModelCheckpoint
 from keras.optimizers import Adam
 
 from preprocessors import preprocessor, parsers, transforms
+
 from generators.resnet_generator import ResnetGenerator
 from generators.cad_generator import CadGenerator
 from generators.augmented_generator import AugmentedGenerator
 
 from models.resnet3d import Resnet3DBuilder
 from models.autoencoder import Cad3dBuilder
+from models.alexnet2d import AlexNet2DBuilder
 
 
 def train_resnet():
@@ -27,12 +29,12 @@ def train_resnet():
     label_loc = '/home/shared/data/elvos_meta_drop1.xls'
 
     # Generators
-    training_gen = AugmentedGenerator(data_loc, label_loc, 
+    training_gen = AugmentedGenerator(data_loc, label_loc,
                                       dims=(dim_len, dim_len, top_len),
                                       batch_size=batch_size)
-    validation_gen = AugmentedGenerator(data_loc, label_loc, 
+    validation_gen = AugmentedGenerator(data_loc, label_loc,
                                         dims=(dim_len, dim_len, top_len),
-                                        batch_size=batch_size, 
+                                        batch_size=batch_size,
                                         validation=True)
 
     # Build and run model
@@ -42,6 +44,47 @@ def train_resnet():
                   metrics=['accuracy'])
     mc_callback = ModelCheckpoint(filepath='tmp/weights.hdf5', verbose=1)
     # tb_callback = TensorBoard(write_images=True)
+
+    print('Model has been compiled.')
+    model.fit_generator(
+        generator=training_gen.generate(),
+        steps_per_epoch=training_gen.get_steps_per_epoch(),
+        validation_data=validation_gen.generate(),
+        validation_steps=validation_gen.get_steps_per_epoch(),
+        epochs=epochs,
+        callbacks=[mc_callback],
+        verbose=1,
+        max_queue_size=1)
+    print('Model has been fit.')
+
+
+def train_alexnet2d():
+    # Parameters
+    dim_len = 200
+    top_len = 24
+    epochs = 10
+    batch_size = 16
+
+    data_loc = '/home/shared/data/data-20180405'
+    label_loc = '/home/shared/data/elvos_meta_drop1.xls'
+
+    # Generators
+    training_gen = AugmentedGenerator(data_loc, label_loc,
+                                      dims=(dim_len, dim_len, top_len),
+                                      batch_size=batch_size,
+                                      extend_dims=False)
+    validation_gen = AugmentedGenerator(data_loc, label_loc,
+                                        dims=(dim_len, dim_len, top_len),
+                                        batch_size=batch_size,
+                                        extend_dims=False,
+                                        validation=True)
+
+    # Build and run model
+    model = AlexNet2DBuilder.build((dim_len, dim_len, top_len))
+    model.compile(optimizer=Adam(lr=0.0001),
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+    mc_callback = ModelCheckpoint(filepath='tmp/alex_weights.hdf5', verbose=1)
 
     print('Model has been compiled.')
     model.fit_generator(
@@ -68,7 +111,7 @@ def train_cad():
                                   validation=True)
 
     # Build and run model
-    model = Cad3dBuilder.build((200, 200, 200,  1), 
+    model = Cad3dBuilder.build((200, 200, 200,  1),
                                filters=(8, 8, 8))
     model.compile(optimizer='adam',
                   loss='mse',
