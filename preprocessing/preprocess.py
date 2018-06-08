fcc0952ab9cbf2ba1924a57519c06f44860361b8
luke-zhu <luke_zhu@brown.edu>
2018-06-08 15:28:46 -0400

    Fix flake8 error in preprocess.py

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -118,7 +118,6 @@ def create_labels_csv(bucket, positives_df, negatives_df) -> None:
     logging.info(f'label value counts {labels_df["label"].value_counts()}')
 
 
-
 def preprocess_scan(slices: List[pydicom.FileDataset]) -> np.array:
     """Transforms the input dicom slices into a numpy array of pixels
     in Hounsfield units with standardized spacing.
