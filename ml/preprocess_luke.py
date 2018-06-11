79a2414aa594c5669e2331a0c2b2ac9b124765bd
luke-zhu <luke_zhu@brown.edu>
2018-06-11 16:26:23 -0400

    Add unit tests for preprocess_luke

--- ml/preprocess_luke.py
+++ ml/preprocess_luke.py
@@ -12,7 +12,6 @@ import random
 import numpy as np
 import pandas as pd
 from google.cloud import storage
-from mayavi import mlab
 from scipy import misc
 
 # TODO: Update this when new data comes.
@@ -513,12 +512,11 @@ def upload_png(arr: np.ndarray, dirname: str, bucket: storage.Bucket):
             logging.error(f'for dirname: {dirname}: {e}')
 
 
-# TODO: Unit test this
 def crop(image3d: np.ndarray) -> np.ndarray:
-    # Update the numbers below to the region is correct
+    """Crops a 3d image in ijk form."""
     lw_center = image3d.shape[1] // 2
-    lw_min = lw_center - 140
-    lw_max = lw_center + 140
+    lw_min = lw_center - 60
+    lw_max = lw_center + 60
     height_max = len(image3d) - 45
     height_min = height_max - 64
     return image3d[height_min:height_max, lw_min:lw_max, lw_min:lw_max]
@@ -563,8 +561,6 @@ if __name__ == '__main__':
         input_arr = download_array(in_blob)
         try:
             output_arr = process_array(input_arr)
-            mlab.contour3d(output_arr)
-            mlab.show()
             filename = in_blob.name.split('/')[-1]
             if filename in TRAINING_LIST:
                 blob_name = f'processed/luke/training/{filename}'
