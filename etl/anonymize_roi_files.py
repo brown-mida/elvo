37766ece936bf0af5d0deae11d739004e8aba5ea
Mary Dong <mdong@as-macbook-pro.devices.brown.edu>
2018-06-11 13:07:25 -0400

    script that changes dropbox folder names

---
+++ etl/anonymize_roi_files.py
@@ -0,0 +1,46 @@
+"""rename the numeric files in the Dropbox Folder "ROI_cropped"
+to the corresponding anonymized alphanumber ID found in sheet 3 of
+Google Drive folder
+
+Note: since this is a one-time change, I (Mary) just hard-coded the path
+according to my laptop's directory structure.
+"""
+
+import csv, os
+# open and store the csv file
+IDs = {}
+with open('/Users/mdong/Desktop/ELVO_Key - Sheet3.csv','rb') as csvfile:
+    timeReader = csv.reader(csvfile, delimiter = ',')
+    # build dictionary with associated IDs
+    for row in timeReader:
+        IDs[row[0]] = row[1]
+
+# move files
+path = '/Users/mdong/Dropbox (Brown)/ROI_ELVOs/ROI_cropped'
+tmpPath = '/Users/mdong/Dropbox (Brown)/ROI_ELVOs/ROI_cropped_anon'
+for oldname in os.listdir(path):
+    # ignore files in path which aren't in the csv file
+    if oldname in IDs:
+        try:
+            os.rename(os.path.join(path, oldname), os.path.join(tmpPath, IDs[oldname]))
+        except:
+            print('File ' + oldname + ' could not be renamed to ' + IDs[oldname] + '!')
+    elif oldname.replace("_1", "") in IDs:
+        oldname_wo_number = oldname.replace("_1", "")
+        try:
+            os.rename(os.path.join(path, oldname), os.path.join(tmpPath, (IDs[oldname_wo_number] + "_1")))
+        except:
+            print('File ' + oldname + ' could not be renamed to ' + IDs[oldname_wo_number] + "_1" + '!')
+    elif oldname.replace("_2", "") in IDs:
+        oldname_wo_number = oldname.replace("_2", "")
+        try:
+            os.rename(os.path.join(path, oldname), os.path.join(tmpPath, (IDs[oldname_wo_number] + "_2")))
+        except:
+            print('File ' + oldname + ' could not be renamed to ' + IDs[oldname_wo_number] + "_2" + '!')
+
+    elif oldname.replace("_3", "") in IDs:
+        oldname_wo_number = oldname.replace("_3", "")
+        try:
+            os.rename(os.path.join(path, oldname), os.path.join(tmpPath, (IDs[oldname_wo_number] + "_3")))
+        except:
+            print('File ' + oldname + ' could not be renamed to ' + IDs[oldname_wo_number] + "_3" + '!')
