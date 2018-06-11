48bfa004732393954c5440a239a657d8da7d94ac
luke-zhu <luke_zhu@brown.edu>
2018-06-10 20:46:56 -0400

    Add ML folder and READMEs for where the source code goes

--- etl/lib/preprocess_test.py
+++ etl/lib/preprocess_test.py
@@ -4,9 +4,9 @@ import pytest as pytest
 @pytest.mark.skip(reason='Requires a file')
 def test_process_cab():
     # preprocess.py is a script so we can't import it without path isses
-    import preprocess_stage1
+    import preprocess
     filepath = '/Users/lukezhu/Desktop/research/elvoai/data/' \
                'RITRAQNET_10.137.213.144_20180531151240195/' \
                '1.3.12.2.1107.5.1.4.66457.30000018012411161725100018246'
-    arr = preprocess_stage1._process_cab(filepath)
+    arr = preprocess._process_cab(filepath)
     print(arr.shape)
