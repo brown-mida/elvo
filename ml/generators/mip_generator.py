372667fc7c95ad33ef9752b2e279f706f7e056b0
Hal Triedman <harold_triedman@brown.edu>
2018-06-19 16:02:34 -0400

    Fixing credentialing to work on jupyter notebook

--- ml/generators/mip_generator.py
+++ ml/generators/mip_generator.py
@@ -41,7 +41,7 @@ class MipGenerator(object):
 
         # Get npy files from Google Cloud Storage
         gcs_client = storage.Client.from_service_account_json(
-            'credentials/client_secret.json'
+            '../credentials/client_secret.json'
         )
         bucket = gcs_client.get_bucket('elvos')
         blobs = bucket.list_blobs(prefix='multichannel_mip_data/from_numpy/')
