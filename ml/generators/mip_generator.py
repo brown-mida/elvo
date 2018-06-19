f13055e8c0553e31cae18e480a4949e71328d189
Hal Triedman <harold_triedman@brown.edu>
2018-06-19 16:42:27 -0400

    Got rid of print lines in MIP generator

--- ml/generators/mip_generator.py
+++ ml/generators/mip_generator.py
@@ -110,7 +110,7 @@ class MipGenerator(object):
         steps = self.get_steps_per_epoch()
         while True:
             for i in range(steps):
-                print(i)
+                # print(i)
                 x, y = self.__data_generation(i)
                 yield x, y
 
@@ -137,8 +137,8 @@ class MipGenerator(object):
             # print(np.shape(img))
             images.append(img)
         images = np.array(images)
-        print("Loaded entire batch.")
-        print(np.shape(images))
+        # print("Loaded entire batch.")
+        # print(np.shape(images))
         return images, labels
 
     def __transform_images(self, image):
