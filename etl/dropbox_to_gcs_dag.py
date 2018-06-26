a4cadf039571fdd6cd512936895ab0eb0586e117
luke-zhu <luke_zhu@brown.edu>
2018-06-26 16:48:14 -0400

    Add slack operator

--- etl/dropbox_to_gcs_dag.py
+++ etl/dropbox_to_gcs_dag.py
@@ -1,7 +1,9 @@
+import os
 from datetime import datetime
 
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.slack_operator import SlackAPIPostOperator
 
 default_args = {
     'owner': 'luke',
@@ -13,3 +15,15 @@ dag = DAG(dag_id='dropbox_to_gcs', default_args=default_args)
 op = BashOperator(task_id='dropbox_to_gcs_op',
                   bash_command='python3 /home/lukezhu/elvo-analysis/etl/dropbox_to_gcs.py',
                   dag=dag)
+
+notify_slack = SlackAPIPostOperator(
+    task_id='notify_slack',
+    channel='i-utra',
+    username='airflow',
+    token=os.environ['SLACK_TOKEN'],
+    text=f'Dropbox to GCS workflow run. '
+         f'Check the Airflow UI to see the results',
+    dag=dag,
+)
+
+op >> notify_slack
