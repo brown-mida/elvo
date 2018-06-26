876354b85ec6d39ffaa29d3155b916a5afb07cb3
Hal Triedman <harold_triedman@brown.edu>
2018-06-26 17:20:54 -0400

    added sagittal transform

--- etl/lib/axial_to_sagittal.py
+++ etl/lib/axial_to_sagittal.py
@@ -1,6 +1,6 @@
 import logging
 import numpy as np
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 from tensorflow.python.lib.io import file_io
 import cloud_management as cloud
 
@@ -19,9 +19,6 @@ if __name__ == '__main__':
     bucket = client.get_bucket('elvos')
 
     for in_blob in bucket.list_blobs(prefix='numpy'):
-        if in_blob.name < 'numpy/ABPO2BORDNF3OVL3.npy':
-            continue
-
         # blacklist
         if in_blob.name == 'numpy/LAUIHISOEZIM5ILF.npy':
             continue
@@ -29,18 +26,21 @@ if __name__ == '__main__':
         # perform the normal MIPing procedure
         logging.info(f'downloading {in_blob.name}')
         axial = cloud.download_array(in_blob)
-        coronal = np.transpose(axial, (1, 0, 2))
-        coronal = np.fliplr(coronal)
+        sagittal = np.transpose(axial, (2, 0, 1))
+        sagittal = np.fliplr(sagittal)
 
         file_id = in_blob.name.split('/')[1]
         file_id = file_id.split('.')[0]
 
+        for i in range(0, len(sagittal), 10):
+            plt.figure(figsize=(6, 6))
+            plt.imshow(sagittal[i], interpolation='none')
+            plt.show()
+
         try:
-            print(f'gs://elvos/mip_data/from_numpy/coronal/{file_id}.npy')
+            print(f'gs://elvos/mip_data/from_numpy/sagittal/{file_id}.npy')
             np.save(file_io.FileIO(f'gs://elvos/mip_data/from_numpy/'
-                                   f'coronal/{file_id}.npy', 'w'), coronal)
-            np.save(file_io.FileIO(f'gs://elvos/mip_data/from_numpy/'
-                                   f'axial/{file_id}.npy', 'w'), axial)
+                                   f'sagittal/{file_id}.npy', 'w'), sagittal)
         except Exception as e:
             logging.error(f'for patient ID: {file_id} {e}')
         logging.info(f'saved .npy file to cloud')
