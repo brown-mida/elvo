04197505d91d541edde7e74471b2c48154d9cadd
luke-zhu <luke_zhu@brown.edu>
2018-06-28 12:07:53 -0400

    Split etl/preprocess into two files
    Also update output dir to raw_numpy/

--- etl/dicom_to_npy.py
+++ etl/dicom_to_npy.py
@@ -13,14 +13,14 @@ import subprocess
 from typing import List
 
 import numpy as np
-import pandas as pd
 import pydicom
 from google.cloud import storage
 
 from lib import parsers
 from lib import transforms
 
-ELVOS_ANON = 'ELVOs_anon'
+IN_DIR = 'ELVOs_anon/'
+OUT_DIR = 'raw_numpy/'
 EXTENSION_LENGTH = len('.cab')  # == 4
 
 
@@ -32,21 +32,6 @@ def configure_logger():
         fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
     root_logger.addHandler(handler)
-
-
-def load_metadata(bucket):
-    """Loads the metadata from GCS.
-    """
-    positives_blob = bucket.get_blob('metadata/positives.csv')
-    positives_bytes = positives_blob.download_as_string()
-    positives_str = positives_bytes.decode('utf-8')
-    positives_df = pd.read_csv(io.StringIO(positives_str))
-
-    negatives_blob = bucket.get_blob('metadata/negatives.csv')
-    negatives_bytes = negatives_blob.download_as_string()
-    negatives_str = negatives_bytes.decode('utf-8')
-    negatives_df = pd.read_csv(io.StringIO(negatives_str))
-    return positives_df, negatives_df
 
 
 def process_cab(blob: storage.Blob, patient_id: str) -> None:
@@ -93,41 +78,13 @@ def process_zip(blob: storage.Blob, patient_id: str) -> None:
 
 
 def save_to_gcs(processed_scan, patient_id, bucket):
-    outpath = f'numpy/{patient_id}.npy'
+    outpath = f'{OUT_DIR}{patient_id}.npy'
     stream = io.BytesIO()
     np.save(stream, processed_scan)
     processed_blob = storage.Blob(outpath, bucket=bucket)
     stream.seek(0)
     processed_blob.upload_from_file(stream)
     logging.info(f'saving dicom data to GCS in {outpath}')
-
-
-def create_labels_csv(bucket, positives_df, negatives_df) -> None:
-    """Creates a file labels.csv mapping patient_id to 1, if positive
-    and 0, if negative.
-    """
-    labels = []
-    blob: storage.Blob
-    for blob in bucket.list_blobs(prefix=ELVOS_ANON + '/'):
-        if blob.name.endswith('.csv'):
-            continue  # Ignore the metadata CSV
-
-        patient_id = blob.name[len(ELVOS_ANON) + 1: -EXTENSION_LENGTH]
-
-        if patient_id in positives_df['Anon ID'].values:
-            labels.append((patient_id, 1))
-        elif patient_id in negatives_df['Anon ID'].values:
-            labels.append((patient_id, 0))
-        else:
-            logging.warning(f'blob with patient id {patient_id} not found in'
-                            f' the metadata CSVs, defaulting to negative')
-            labels.append((patient_id, 0))
-    labels_df = pd.DataFrame(labels, columns=['patient_id', 'label'])
-    labels_df.to_csv('labels.csv', index=False)
-    logging.info('uploading labels to the gs://elvos/labels.csv')
-    labels_blob = storage.Blob('labels.csv', bucket=bucket)
-    labels_blob.upload_from_filename('labels.csv')
-    logging.info(f'label value counts {labels_df["label"].value_counts()}')
 
 
 def preprocess_scan(slices: List[pydicom.FileDataset]) -> np.array:
@@ -143,30 +100,21 @@ def main():
     gcs_client = storage.Client(project='elvo-198322')
     input_bucket = gcs_client.get_bucket('elvos')
 
-    positives_df, negatives_df = load_metadata(input_bucket)
-    create_labels_csv(input_bucket, positives_df, negatives_df)
-
     blob: storage.Blob
-    for blob in input_bucket.list_blobs(prefix=ELVOS_ANON + '/'):
+    for blob in input_bucket.list_blobs(prefix=IN_DIR):
         if blob.name.endswith('.csv'):
             continue  # Ignore the metadata CSV
 
-        try:
-            logging.info(f'processing blob {blob.name}')
-            patient_id = blob.name[len(ELVOS_ANON) + 1: -EXTENSION_LENGTH]
+        logging.info(f'processing blob {blob.name}')
+        patient_id = blob.name[len(IN_DIR): -EXTENSION_LENGTH]
 
-            if blob.name.endswith('.cab'):
-                process_cab(blob, patient_id)
-            elif blob.name.endswith('.zip'):
-                process_zip(blob, patient_id)
-            else:
-                logging.info(f'file extension must be .cab or .zip,'
-                             f' got {blob.name}')
-        except Exception as e:
-            # Reset the working directory, tmp files for the next dataset
-            os.chdir('..')
-            shutil.rmtree('tmp')
-            logging.error(e)
+        if blob.name.endswith('.cab'):
+            process_cab(blob, patient_id)
+        elif blob.name.endswith('.zip'):
+            process_zip(blob, patient_id)
+        else:
+            logging.info(f'file extension must be .cab or .zip,'
+                         f' got {blob.name}')
 
 
 if __name__ == '__main__':
