cf3639a1c2e9489a4abda232e670646894de3478
luke-zhu <luke_zhu@brown.edu>
2018-07-02 23:05:40 -0400

    Refactor resnet_train.py

--- notebooks/gpu1708/resnet_train.py
+++ notebooks/gpu1708/resnet_train.py
@@ -1,7 +1,8 @@
-import os
 import pathlib
+import typing
 
 import numpy as np
+import os
 import pandas as pd
 import tensorflow as tf
 from keras import (
@@ -15,9 +16,7 @@ from keras import backend as K
 from keras.preprocessing import image
 
 
-def load_data(data_dir):
-    """Returns a dictionary which maps patient ids
-    to patient pixel data."""
+def load_data(data_dir: str) -> typing.Dict[str, np.ndarray]:
     data_dict = {}
     for filename in os.listdir(data_dir):
         patient_id = filename[:-4]  # remove .npy extension
@@ -25,7 +24,9 @@ def load_data(data_dir):
     return data_dict
 
 
-def to_arrays(data, labels):
+def to_shuffled_arrays(data: typing.Dict[str, np.ndarray],
+                       labels: pd.DataFrame) -> typing.Tuple[
+    np.ndarray, np.ndarray]:
     shuffled_ids = list(data.keys())
     np.random.shuffle(shuffled_ids)
     X_list = []
@@ -52,6 +53,7 @@ def as_keras_metric(method):
     import functools
     from keras import backend as K
     import tensorflow as tf
+
     @functools.wraps(method)
     def wrapper(self, args, **kwargs):
         """ Wrapper for turning tensorflow metrics into keras metrics """
@@ -60,50 +62,77 @@ def as_keras_metric(method):
         with tf.control_dependencies([update_op]):
             value = tf.identity(value)
         return value
+
     return wrapper
 
 
-def build_resnet_model():
-    resnet = applications.ResNet50(include_top=False, input_shape=(220, 220, 3))
+def build_resnet_model(input_shape,
+                       dropout_rate1=0.5,
+                       dropout_rate2=0.5,
+                       optimizer=optimizers.Adam(lr=1e-5),
+                       metrics=None) -> models.Model:
+    resnet = applications.ResNet50(include_top=False, input_shape=input_shape)
     x = resnet.output
     x = layers.GlobalAveragePooling2D()(x)
     x = layers.Dense(1024, activation='relu')(x)
-    x = layers.Dropout(0.5)(x)
+    x = layers.Dropout(dropout_rate1)(x)
     x = layers.Dense(1024, activation='relu')(x)
-    x = layers.Dropout(0.5)(x)
+    x = layers.Dropout(dropout_rate2)(x)
     predictions = layers.Dense(1, activation='sigmoid')(x)
-    
-#     for layer in resnet.layers:
-#         layer.trainable = False
 
     model = models.Model(resnet.input, predictions)
-    
-    auc_roc = as_keras_metric(tf.metrics.auc)
-    true_positives = as_keras_metric(tf.metrics.true_positives)
-    precision = as_keras_metric(tf.metrics.precision)
-    recall = as_keras_metric(tf.metrics.recall)
-    model.compile(optimizer=optimizers.Adam(lr=1e-5),
+
+    if metrics is None:
+        print('Using default metrics: acc, auc, precision, recall, tp, fn')
+        auc_roc = as_keras_metric(tf.metrics.auc)
+        true_positives = as_keras_metric(tf.metrics.true_positives)
+        false_negatives = as_keras_metric(tf.metrics.false_negatives)
+        precision = as_keras_metric(tf.metrics.precision)
+        recall = as_keras_metric(tf.metrics.recall)
+        metrics = ['acc',
+                   auc_roc,
+                   precision,
+                   recall,
+                   true_positives,
+                   false_negatives]
+
+    model.compile(optimizer=optimizer,
                   loss='binary_crossentropy',
-                  metrics=['acc', auc_roc, precision, recall, true_positives])
+                  metrics=metrics)
 
     return model
 
 
 if __name__ == '__main__':
-    data = load_data('/home/lzhu7/elvo-analysis/data/mip_three16/')
-    labels = pd.read_csv('/home/lzhu7/elvo-analysis/data/labels_mip_three16.csv',
-                         index_col='Anon ID')[['occlusion_exists']]
-    print('seeding to 0')
-    np.random.seed(42)
-    x, y = to_arrays(data, labels)
+    args = {
+        'data_dir': '/home/lzhu7/elvo-analysis/data/mip_three11/',
+        'labels_path': '/home/lzhu7/elvo-analysis/data/labels_mip_three11.csv',
+        'index_col': 'Anon ID',
+        'label_col': 'occlusion_exists',
+        'seed': 42,
+        'split_idx': 900,
+    }
 
-    x_train = x[0:900]
-    y_train = y[0:900]
-    x_valid = x[900:]
-    y_valid = y[900:]
-    print('training positives:', y_train.sum(), 'training negatives', len(y_train) - y_train.sum())
-    print('validation positives:', y_valid.sum(),  'validation negatives', len(y_valid) - y_valid.sum())
-    print('x_train mean:', x_train.mean(), 'x_train std:', x_train.std())
+    data = load_data(args['data_dir'])
+    labels = pd.read_csv(args['labels_path'],
+                         index_col=args['index_col'])[[args['label_col']]]
+
+    print(f'seeding to {args["seed"]}')
+    np.random.seed(args["seed"])
+
+    x, y = to_shuffled_arrays(data, labels)
+
+    x_train = x[0:args['split_idx']]
+    y_train = y[0:args['split_idx']]
+    x_valid = x[args['split_idx']:]
+    y_valid = y[args['split_idx']:]
+
+    print('training positives:', y_train.sum(),
+          'training negatives', len(y_train) - y_train.sum())
+    print('validation positives:', y_valid.sum(),
+          'validation negatives', len(y_valid) - y_valid.sum())
+    print('x_train mean:', x_train.mean(),
+          'x_train std:', x_train.std())
 
     datagen = image.ImageDataGenerator(rotation_range=15,
                                        width_shift_range=0.1,
@@ -111,18 +140,31 @@ if __name__ == '__main__':
                                        zoom_range=[1.0, 1.1],
                                        horizontal_flip=True)
     datagen.fit(x_train)
+
     train_gen = datagen.flow(x_train, y_train, batch_size=48)
     valid_gen = datagen.flow(x_valid, y_valid, batch_size=48)
-    
+
     train_arr = train_gen.next()[0]
     valid_arr = valid_gen.next()[0]
-    print('shape:', train_arr.shape, 'mean:', train_arr.mean(), 'std:', train_arr.std())
-    print('shape:', valid_arr.shape, 'mean:', valid_arr.mean(), 'std:', valid_arr.std())
 
-    model = build_resnet_model()
+    print('train_gen sample batch:',
+          'shape:', train_arr.shape,
+          'mean:', train_arr.mean(),
+          'std:', train_arr.std())
+    print('test_gen sample batch:',
+          'shape:', valid_arr.shape,
+          'mean:', valid_arr.mean(),
+          'std:', valid_arr.std())
+
+    model = build_resnet_model((200, 200, 3))
     model.summary()
-    checkpointer = callbacks.ModelCheckpoint(filepath='weights-13.hdf5',
-                                             verbose=1, save_best_only=True)
+
+    checkpointer = callbacks.ModelCheckpoint(filepath='weights-11.hdf5',
+                                             verbose=1,
+                                             save_best_only=True)
     early_stopper = callbacks.EarlyStopping(patience=10)
-    model.fit_generator(train_gen, epochs=100, validation_data=valid_gen,
+
+    model.fit_generator(train_gen,
+                        epochs=100,
+                        validation_data=valid_gen,
                         callbacks=[checkpointer, early_stopper])
