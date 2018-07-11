57874b444a3ff4ff93233e4e0e84b48a2b7a0dac
Hal Triedman <harold_triedman@brown.edu>
2018-07-11 12:12:49 -0400

    trying to debug fusionnet3d

--- ml/models/fusion_net3d.py
+++ ml/models/fusion_net3d.py
@@ -1,10 +1,8 @@
-from ml.models.model import ModelBuilder
-
 from keras.layers import Input, Conv3D, Dense, MaxPooling3D, Dropout
 from keras.models import Model
 
 
-class FusionNet3dBuilder(ModelBuilder):
+class FusionNet3dBuilder(object):
 
     @staticmethod
     def build(input_shape, num_classes=1):
@@ -13,17 +11,26 @@ class FusionNet3dBuilder(ModelBuilder):
             raise ValueError("Input shape must have 4 channels")
 
         input_img = Input(input_shape)
-        conv1 = Conv3D(64, kernel_size=(3, 3, 64),
-                       activation='relu')(input_img)
+        print(input_img)
+        conv1 = Conv3D(64, kernel_size=(3, 3, 32), activation='relu')(input_img)
+        print(conv1)
         maxpool1 = MaxPooling3D(pool_size=(2, 2, 1))(conv1)
-        conv2 = Conv3D(64, kernel_size=(3, 3, 64),
-                       activation='relu')(maxpool1)
-        conv3 = Conv3D(64, kernel_size=(3, 3, 64),
-                       activation='relu')(conv2)
+        print(maxpool1)
+        conv2 = Conv3D(64, kernel_size=(3, 3, 32), activation='relu')(maxpool1)
+        print(conv2)
+        conv3 = Conv3D(64, 3, 3, 32, activation='relu')(conv2)
+        print(conv3)
         maxpool2 = MaxPooling3D(pool_size=(2, 2, 1))(conv3)
+        print(maxpool2)
         dropout = Dropout(0.5)(maxpool2)
         dense1 = Dense(2048, activation='relu')(dropout)
+        print(dense1)
         output_img = Dense(num_classes, activation='softmax')(dense1)
+        print(output_img)
 
         model = Model(inputs=input_img, outputs=output_img)
         return model
+
+
+model = FusionNet3dBuilder.build((32, 32, 32, 1))
+print(model.summary())
