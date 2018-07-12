8ca0296d1f02812956a972ad524870f5d572be3d
luke-zhu <luke_zhu@brown.edu>
2018-07-12 16:18:26 -0400

    Implement working param grid dataclass

--- ml/blueno/types.py
+++ ml/blueno/types.py
@@ -1,7 +1,10 @@
 import typing
 
+from dataclasses import dataclass
 
-class ModelConfig(typing.NamedTuple):
+
+@dataclass
+class ModelConfig:
     model_callable: typing.Callable
     optimizer: typing.Callable
     loss: typing.Callable
@@ -12,10 +15,11 @@ class ModelConfig(typing.NamedTuple):
     rotation_range: int  # Deprecated
 
 
-# TODO: Implement the generator
-class GeneratorConfig(typing.NamedTuple):
+# TODO: Integrate the generator
+@dataclass
+class GeneratorConfig:
     generator_callable: typing.Callable
-    rotation_range: int
+    rotation_range: int = 30
     width_shift_range: float = 0.1
     height_shift_range: float = 0.1
     shear_range: float = 0
@@ -24,10 +28,26 @@ class GeneratorConfig(typing.NamedTuple):
     vertical_flip = False
 
 
-class ParamGrid(typing.NamedTuple):
-    data: typing.List[str]
-    generator: typing.List[GeneratorConfig]
-    model: typing.List[ModelConfig]
-    seed: typing.List[int] = [0, 42]
-    val_split: typing.List[int] = [0.1, 0.2]
+@dataclass
+class ParamGrid:
+    data: typing.Tuple[str]
+    generator: typing.Tuple[GeneratorConfig]
+    model: typing.Tuple[ModelConfig]
+    seed: typing.Tuple[int] = (0, 1)
+    val_split: typing.Tuple[int] = (0.1, 0.2)
+
+    # TODO: Batch size to paramgrid
     # batch_size: typing.List[int] = None
+
+    def __init__(self, **kwargs):
+        if not isinstance(kwargs['generator'], GeneratorConfig):
+            generators = tuple(
+                GeneratorConfig(**gen) for gen in kwargs['generator'])
+            self.generator = generators
+        if not isinstance(kwargs['model'], ModelConfig):
+            models = tuple(ModelConfig(**mod) for mod in kwargs['model'])
+            self.model = models
+
+        self.data = kwargs['data']
+        self.seed = kwargs['seed']
+        self.val_split = kwargs['val_split']
