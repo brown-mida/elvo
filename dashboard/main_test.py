0309b75e9c8358bb1280f8816943b7d2fb49e582
luke-zhu <luke_zhu@brown.edu>
2018-07-13 01:13:27 -0400

    Update dashboard deployment requirements and config

--- dashboard/main_test.py
+++ dashboard/main_test.py
@@ -14,6 +14,14 @@ def test_index():
     assert r.status_code == 200
 
 
+def test_dimensions():
+    from main import app
+    app.testing = True
+    client = app.test_client()
+    r = client.get('/image/dimensions/L66E2921S3O1MURX')
+    assert r.status_code == 200
+
+
 @pytest.mark.skipif('TRAVIS' in os.environ, reason='Database not on travis')
 def test_roi():
     from main import app
