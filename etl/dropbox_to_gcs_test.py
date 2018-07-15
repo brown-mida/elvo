03872b0d4972469cbaea47de61e75d8fa8c4ad24
luke-zhu <luke_zhu@brown.edu>
2018-07-15 10:59:39 -0400

    Use update timestamps to make sure that ELVO data is not outdated

--- etl/dropbox_to_gcs_test.py
+++ etl/dropbox_to_gcs_test.py
@@ -15,4 +15,4 @@ def test_upload_entry():
     bucket = gcs_client.get_bucket('elvos')
 
     entry = dbx.files_get_metadata('/ELVOs_anon/068WBWCQGW5JHBYV.cab')
-    dropbox_to_gcs.upload_entry(entry, dbx, bucket)
+    dropbox_to_gcs.upload_entry_if_outdated(entry, dbx, bucket)
