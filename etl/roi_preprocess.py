d932ea60da00ca07d19e652057e2e6ed784e8632
Hal Triedman <harold_triedman@brown.edu>
2018-07-16 12:23:04 -0400

    GOT THIS SHIT TO WOORK WOOOOOOOO

--- etl/roi_preprocess.py
+++ etl/roi_preprocess.py
@@ -22,6 +22,8 @@ def create_chunks(annotations_df: pd.DataFrame):
     # loop through every array on GCS
     for in_blob in bucket.list_blobs(prefix='airflow/npy'):
         # blacklist
+        if in_blob.name != 'airflow/npy/04IOS24JP70LHBGB.npy':
+            continue
         if in_blob.name == 'airflow/npy/LAUIHISOEZIM5ILF.npy':
             continue
 
@@ -46,119 +48,20 @@ def create_chunks(annotations_df: pd.DataFrame):
         # if it's elvo positive
         if elvo_positive:
             h = 0
+            blue = int(len(arr) - roi_df['blue2'].iloc[0])
+            green = int(roi_df['green1'].iloc[0])
+            red = int(roi_df['red1'].iloc[0])
             # loop through every chunk
-            for i in range(int(roi_df['blue1'].iloc[0]) % 32, len(arr), 32):
-                for j in range(int(roi_df['green1'].iloc[0]) % 32, len(arr[0]), 32):
-                    for k in range(int(roi_df['red1'].iloc[0]) % 32, len(arr[0][0]), 32):
+            for i in range(blue % 32, len(arr), 32):
+                for j in range(green % 32, len(arr[0]), 32):
+                    for k in range(red % 32, len(arr[0][0]), 32):
 
                         elvo_chunk = False
 
                         # if the chunk contains the elvo, set a boolean to true
-                        if i == int(roi_df['blue1'].iloc[0]) \
-                                and j == int(roi_df['green1'].iloc[0]) \
-                                and k == int(roi_df['red1'].iloc[0]):
-                            chunk = arr[i:(i + 32), j:(j + 32), k:(k + 32)]
+                        if i == blue and j == green and k == red:
                             elvo_chunk = True
-                            chunk[chunk < 75] = 0
-                            chunk[chunk > 400] = 0
-                            axial = np.max(chunk, axis=0)
-                            coronal = np.max(chunk, axis=1)
-                            sag = np.max(chunk, axis=2)
-                            fig, ax = plt.subplots(1, 3, figsize=(6, 10))
-                            ax[0].imshow(axial, interpolation='none')
-                            ax[1].imshow(coronal, interpolation='none')
-                            ax[2].imshow(sag, interpolation='none')
-                            fig.tight_layout()
-                            plt.show()
 
-                         # if the chunk contains the elvo, set a boolean to true
-                        elif i == int(roi_df['blue1'].iloc[0]) \
-                                    and j == int(roi_df['red1'].iloc[0]) \
-                                    and k == int(roi_df['green1'].iloc[0]):
-                            chunk = arr[i:(i + 32), j:(j + 32), k:(k + 32)]
-                            elvo_chunk = True
-                            chunk[chunk < 75] = 0
-                            chunk[chunk > 400] = 0
-                            axial = np.max(chunk, axis=0)
-                            coronal = np.max(chunk, axis=1)
-                            sag = np.max(chunk, axis=2)
-                            fig, ax = plt.subplots(1, 3, figsize=(6, 10))
-                            ax[0].imshow(axial, interpolation='none')
-                            ax[1].imshow(coronal, interpolation='none')
-                            ax[2].imshow(sag, interpolation='none')
-                            fig.tight_layout()
-                            plt.show()
-
-                        elif i == int(roi_df['green1'].iloc[0]) \
-                                and j == int(roi_df['blue1'].iloc[0]) \
-                                 and k == int(roi_df['red1'].iloc[0]):
-                            elvo_chunk = True
-                            chunk = arr[i:(i + 32), j:(j + 32), k:(k + 32)]
-                            elvo_chunk = True
-                            chunk[chunk < 75] = 0
-                            chunk[chunk > 400] = 0
-                            axial = np.max(chunk, axis=0)
-                            coronal = np.max(chunk, axis=1)
-                            sag = np.max(chunk, axis=2)
-                            fig, ax = plt.subplots(1, 3, figsize=(6, 10))
-                            ax[0].imshow(axial, interpolation='none')
-                            ax[1].imshow(coronal, interpolation='none')
-                            ax[2].imshow(sag, interpolation='none')
-                            fig.tight_layout()
-                            plt.show()
-
-                        elif i == int(roi_df['green1'].iloc[0]) \
-                                 and j == int(roi_df['red1'].iloc[0]) \
-                                 and k == int(roi_df['blue1'].iloc[0]):
-                            elvo_chunk = True
-                            chunk = arr[i:(i + 32), j:(j + 32), k:(k + 32)]
-                            chunk[chunk < 75] = 0
-                            chunk[chunk > 400] = 0
-                            axial = np.max(chunk, axis=0)
-                            coronal = np.max(chunk, axis=1)
-                            sag = np.max(chunk, axis=2)
-                            fig, ax = plt.subplots(1, 3, figsize=(6, 10))
-                            ax[0].imshow(axial, interpolation='none')
-                            ax[1].imshow(coronal, interpolation='none')
-                            ax[2].imshow(sag, interpolation='none')
-                            fig.tight_layout()
-                            plt.show()
-
-                        elif i == int(roi_df['red1'].iloc[0]) \
-                                 and j == int(roi_df['green1'].iloc[0]) \
-                                 and k == int(roi_df['blue1'].iloc[0]):
-                            chunk = arr[i:(i + 32), j:(j + 32), k:(k + 32)]
-                            elvo_chunk = True
-                            chunk[chunk < 75] = 0
-                            chunk[chunk > 400] = 0
-                            axial = np.max(chunk, axis=0)
-                            coronal = np.max(chunk, axis=1)
-                            sag = np.max(chunk, axis=2)
-                            fig, ax = plt.subplots(1, 3, figsize=(6, 10))
-                            ax[0].imshow(axial, interpolation='none')
-                            ax[1].imshow(coronal, interpolation='none')
-                            ax[2].imshow(sag, interpolation='none')
-                            fig.tight_layout()
-                            plt.show()
-
-                        elif i == int(roi_df['red1'].iloc[0]) \
-                                 and j == int(roi_df['blue1'].iloc[0]) \
-                                 and k == int(roi_df['green1'].iloc[0]):
-                            elvo_chunk = True
-                            chunk = arr[i:(i + 32), j:(j + 32), k:(k + 32)]
-                            chunk[chunk < 75] = 0
-                            chunk[chunk > 400] = 0
-                            axial = np.max(chunk, axis=0)
-                            coronal = np.max(chunk, axis=1)
-                            sag = np.max(chunk, axis=2)
-                            fig, ax = plt.subplots(1, 3, figsize=(6, 10))
-                            ax[0].imshow(axial, interpolation='none')
-                            ax[1].imshow(coronal, interpolation='none')
-                            ax[2].imshow(sag, interpolation='none')
-                            fig.tight_layout()
-                            plt.show()
-
-                        continue
                         # copy the chunk
                         chunk = arr[i:(i + 32), j:(j + 32), k:(k + 32)]
 
@@ -174,13 +77,13 @@ def create_chunks(annotations_df: pd.DataFrame):
                             #   1 and save
                             if elvo_chunk:
                                 cloud.save_chunks_to_cloud(np.asarray(chunk),
-                                                           'normal/positive',
+                                                           'normal', 'positive',
                                                            file_id + str(h))
 
                             # else set the label to 0 and save
                             else:
                                 cloud.save_chunks_to_cloud(np.asarray(chunk),
-                                                           'normal/negative',
+                                                           'normal', 'negative',
                                                            file_id + str(h))
 
                         # # do the same thing with stripped array
@@ -224,12 +127,9 @@ def create_chunks(annotations_df: pd.DataFrame):
                         airspace = np.where(chunk < -300)
                         # if it's less than 90% airspace
                         if (airspace[0].size / chunk.size) < 0.9:
-                            # cloud.save_chunks_to_cloud(np.asarray(chunk),
-                            #                            'normal',
-                            #                            file_id + str(h))
-                            # # save the label as 0 and save it to the cloud
+                            # save the label as 0 and save it to the cloud
                             cloud.save_chunks_to_cloud(np.asarray(chunk),
-                                                       'normal/negative',
+                                                       'normal', 'negative',
                                                        file_id + str(h))
 
                         # # do the same thing for stripped
@@ -283,22 +183,23 @@ def create_labels(annotations_df: pd.DataFrame):
 
         # if it's elvo positive
         if elvo_positive:
+            blue = int(len(arr) - roi_df['blue2'].iloc[0])
+            green = int(roi_df['green1'].iloc[0])
+            red = int(roi_df['red1'].iloc[0])
             h = 0
             # loop through every chunk
-            for i in range(int(roi_df['blue1'].iloc[0]) % 32, len(arr), 32):
-                for j in range(int(roi_df['green1'].iloc[0]) % 32, len(arr[0]), 32):
-                    for k in range(int(roi_df['red1'].iloc[0]) % 32, len(arr[0][0]), 32):
+            for i in range(blue % 32, len(arr), 32):
+                for j in range(green % 32, len(arr[0]), 32):
+                    for k in range(red % 32, len(arr[0][0]), 32):
 
                         elvo_chunk = False
-
-                        # if the chunk contains the elvo, set a boolean to true
-                        if i == int(roi_df['blue1'].iloc[0]) \
-                                and j == int(roi_df['green1'].iloc[0]) \
-                                and k == int(roi_df['red1'].iloc[0]):
-                            elvo_chunk = True
-
                         # copy the chunk
                         chunk = arr[i:(i + 32), j:(j + 32), k:(k + 32)]
+
+                        # if the chunk contains the elvo, set a boolean to true
+                        if i == blue and j == green and k == red:
+                            elvo_chunk = True
+
                         # calculate airspace
                         airspace = np.where(chunk < -300)
                         # if the chunk is more than 90% airspace
@@ -336,16 +237,9 @@ def create_labels(annotations_df: pd.DataFrame):
     labels_df.to_csv('annotated_labels.csv')
 
 
-def pd_test():
-    label_dict = {'04IOS24JP70LHBGB182': 0,
-                  '04IOS24JP70LHBGB183': 0,
-                  '04IOS24JP70LHBGB184': 1,
-                  '04IOS24JP70LHBGB189': 0}
-    print(pd.DataFrame.from_dict(label_dict, orient='index', columns=['label']))
-
-
 def process_labels():
-    annotations_df = pd.read_csv('/home/harold_triedman/elvo-analysis/annotation.csv')
+    # annotations_df = pd.read_csv('/home/harold_triedman/elvo-analysis/annotation.csv')
+    annotations_df = pd.read_csv('/Users/haltriedman/Desktop/annotations.csv')
     annotations_df = annotations_df.drop(['created_by',
                                           'created_at',
                                           'ROI Link',
@@ -357,8 +251,58 @@ def process_labels():
     return annotations_df
 
 
+def inspect_rois(annotations_df):
+    client = cloud.authenticate()
+    bucket = client.get_bucket('elvos')
+
+    # loop through every array on GCS
+    for in_blob in bucket.list_blobs(prefix='airflow/npy'):
+        # if in_blob.name != 'airflow/npy/ZZX0ZNWG6Q9I18GK.npy':
+        #     continue
+        # blacklist
+        if in_blob.name == 'airflow/npy/LAUIHISOEZIM5ILF.npy':
+            continue
+
+        # get the file id
+        file_id = in_blob.name.split('/')[2]
+        file_id = file_id.split('.')[0]
+
+        print(f'chunking {file_id}')
+        # copy ROI if there's a positive match in the ROI annotations
+        roi_df = annotations_df[annotations_df['patient_id'].str.match(file_id)]
+        # if it's empty, this brain is ELVO negative
+        if roi_df.empty:
+            elvo_positive = False
+        else:
+            elvo_positive = True
+
+        arr = cloud.download_array(in_blob)
+
+        # if it's elvo positive
+        if elvo_positive:
+            chunks = []
+            blue = int(len(arr) - roi_df['blue2'].iloc[0])
+            green = int(roi_df['green1'].iloc[0])
+            red = int(roi_df['red1'].iloc[0])
+            chunks.append(arr[blue: blue + 32, green: green + 50, red: red + 50])
+            chunks.append(arr[blue: blue + 32, red: red + 50, green: green + 50])
+            start = 0
+            for chunk in chunks:
+                print(start)
+                axial = np.max(chunk, axis=0)
+                coronal = np.max(chunk, axis=1)
+                sag = np.max(chunk, axis=2)
+                fig, ax = plt.subplots(1, 3, figsize=(6, 4))
+                ax[0].imshow(axial, interpolation='none')
+                ax[1].imshow(coronal, interpolation='none')
+                ax[2].imshow(sag, interpolation='none')
+                plt.show()
+                start += 10
+
+
 if __name__ == '__main__':
     configure_logger()
     annotations_df = process_labels()
-    # create_labels(annotations_df)
+    create_labels(annotations_df)
     create_chunks(annotations_df)
+    # inspect_rois(annotations_df)
