930c07185bd66fe0e9f18f0d359e880d6b8f7217
amwa <amyw98@comcast.net>
2018-07-16 22:16:23 +0000

    more bug fixes

--- etl/transform_positives.py
+++ etl/transform_positives.py
@@ -6,10 +6,11 @@ storage folder "positives + augmentation."
 
 import logging
 # from matplotlib import pyplot as plt
-from lib import transforms, cloud_management as cloud
+from lib import cloud_management as cloud
 import numpy as np
 import pandas as pd
 import itertools
+
 
 def configure_logger():
     root_logger = logging.getLogger()
@@ -20,8 +21,9 @@ def configure_logger():
     handler.setFormatter(formatter)
     root_logger.addHandler(handler)
 
+
 def transform_one(arr, file_id):
-    iterlist = list(itertools.product('01', repeat = 3))
+    iterlist = list(itertools.product('01', repeat=3))
     axes = [[0, 1], [1, 2], [0, 2]]
 
     transform_number = 0
@@ -41,6 +43,7 @@ def transform_one(arr, file_id):
             # print(file_id_new)
             cloud.save_chunks_to_cloud(flipped, 'normal',
                                        'positive', file_id_new)
+
 
 def transform_positives():
     configure_logger()
@@ -64,11 +67,12 @@ def transform_positives():
         input_arr = cloud.download_array(in_blob)
         logging.info(f"blob shape: {input_arr.shape}")
         # crop individual input array
-        flipped = transform_one(input_arr, file_id)
+        transform_one(input_arr, file_id)
 
         # plt.figure(figsize=(6, 6))
         # plt.imshow(not_extreme_arr, interpolation='none')
         # plt.show()
+
 
 def clean_data():
     configure_logger()
@@ -104,14 +108,15 @@ def generate_csv():
     labels_df = pd.read_csv('/home/amy/data/annotated_labels.csv')
     for index, row in labels_df.iterrows():
         print(index, row[1])
-        if(row[1]== 1):
+        if(row[1] == 1):
             # every time you come across a positive, add in 24 more rows
             to_add = {}
             for i in range(24):
                 # we use index + 1 because filenames are 1-indexed
                 new_patient_id = str(row[0]) + "_" + str(i + 1)
                 to_add[index + 500000 + i] = [new_patient_id, 1]
-            to_add_df = pd.DataFrame.from_dict(to_add, orient='index', columns =['Unnamed: 0', 'label'])
+            to_add_df = pd.DataFrame.from_dict(
+                to_add, orient='index', columns=['Unnamed: 0', 'label'])
             print(to_add_df)
             labels_df = labels_df.append(to_add_df)
     # print(labels_df.loc['04IOS24JP70LHBGB184', 'Unnamed: 0'])
@@ -125,3 +130,4 @@ if __name__ == '__main__':
     # generate_csv()
     # transform_positives()
     clean_data()
+    
