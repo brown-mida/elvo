da8d36487d8f47b687c066e1940647ad2e4b803a
Hal Triedman <harold_triedman@brown.edu>
2018-07-18 16:54:05 -0400

    added beginning of hyperparam optimization script for c3d model and synced it up to slack

--- ml/c3d_train.py
+++ ml/c3d_train.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
-from ml.models.three_d import c3d
-from ml.blueno.reporting import slack_
+from models.three_d import c3d
+from blueno.slack import slack_report
+from blueno import utils
 import os
 import csv
 import numpy as np
@@ -9,9 +10,7 @@ from google.cloud import storage
 from etl.lib import cloud_management
 
 from keras.optimizers import SGD
-from keras import metrics
 from keras import backend as K
-from keras.callbacks import ModelCheckpoint
 
 BLACKLIST = []
 LEARN_RATE = 1e-5
@@ -111,43 +110,61 @@ chunks, labels = zip(*tmp)
 
 # Split based on validation
 print('splitting based on validation split')
-X_train = np.asarray(chunks[int(len(chunks) * 0.1):])
-y_train = np.asarray(labels[int(len(labels) * 0.1):])
+full_x_train = np.asarray(chunks[int(len(chunks) * 0.1):])
+full_y_train = np.asarray(labels[int(len(labels) * 0.1):])
 
-X_val = np.asarray(chunks[:int(len(chunks) * 0.1)])
-y_val = np.asarray(labels[:int(len(labels) * 0.1)])
+full_x_val = np.asarray(chunks[:int(len(chunks) * 0.1)])
+full_y_val = np.asarray(labels[:int(len(labels) * 0.1)])
 
 print(f'{len(chunks)} total chunks to train with')
 
+print(f'full training data: {full_x_train.shape}, {full_y_train.shape}')
+print(f'full validation data: {full_x_val.shape}, {full_y_val.shape}')
+
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 
-metrics.true_positives = true_positives
-metrics.false_negtives = false_negatives
-metrics.sensitivity = sensitivity
-metrics.specificity = specificity
+metrics = ['acc',
+           utils.true_positives,
+           utils.false_negatives,
+           utils.sensitivity,
+           utils.specificity]
 
-model = c3d.C3DBuilder.build()
-model.compile(optimizer=SGD(lr=LEARN_RATE, momentum=0.9, nesterov=True),
-              loss={"out_class": "binary_crossentropy"},
-#               loss='binary_crossentropy'
-              metrics={"out_class": [metrics.binary_accuracy,
-                                     metrics.binary_crossentropy,
-                                     true_positives,
-                                     false_negatives,
-                                     sensitivity,
-                                     specificity]
-                      }
-             )
+for i in range(1, 11):
 
-print(X_train.shape, y_train.shape)
-print(X_val.shape, y_val.shape)
+    model = c3d.C3DBuilder.build()
+    opt = SGD(lr=LEARN_RATE, momentum=0.9, nesterov=True)
+    model.compile(optimizer=opt,
+                  loss={"out_class": "binary_crossentropy"},
+                  metrics=metrics)
 
-mc_callback = ModelCheckpoint(filepath='tmp/c3d_weights.hdf5', verbose=1)
-model.fit(
-    x=X_train,
-    y=y_train,
-    epochs=100,
-    batch_size=16,
-    callbacks=[mc_callback],
-    validation_data = (X_val, y_val),
-    verbose=1)
+    frac = i / 10
+    X_train = full_x_train[:int(len(full_x_train) * frac)]
+    y_train = full_y_train[:int(len(full_y_train) * frac)]
+    X_val = full_x_val[:int(len(full_x_val) * frac)]
+    y_val = full_y_val[:int(len(full_y_val) * frac)]
+    callbacks = utils.create_callbacks(x_train=X_train,
+                                       y_train=y_train,
+                                       x_valid=X_val,
+                                       y_valid=y_val,
+                                       normalize=False)
+
+    history = model.fit(x=X_train,
+                        y=y_train,
+                        epochs=100,
+                        batch_size=16,
+                        callbacks=callbacks,
+                        validation_data=(X_val, y_val),
+                        verbose=1)
+
+    slack_report(x_train=X_train,
+                 x_valid=X_val,
+                 y_valid=y_val,
+                 model=model,
+                 history=history,
+                 name=f'Basic C3D (training on {frac * 100}% of data)',
+                 params=f'The most basic, non-optimized version of C3D, '
+                        f'training on {frac * 100}% of data',
+                 token='xoxp-314216549302'
+                       '-332571517623'
+                       '-402064251175'
+                       '-cde67240d96f69a3534e5c919ff097e7')
