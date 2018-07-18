6df16baddb8294ae12fabaf4c9cb31fb7ae7462c
Hal Triedman <harold_triedman@brown.edu>
2018-07-18 17:18:23 -0400

    separated ROI data preprocessing and training scripts

--- ml/c3d_train.py
+++ ml/c3d_train.py
@@ -1,104 +1,22 @@
 import tensorflow as tf
-from ml.models.three_d import c3d
-from ml.blueno.slack import slack_report
-from ml.blueno import utils
-import os
-import csv
-import numpy as np
-import random
-from google.cloud import storage
-from etl.lib import cloud_management
+from models.three_d import c3d
+from blueno.slack import slack_report
+from blueno import utils
 from keras.optimizers import SGD
+import pickle
 
 BLACKLIST = []
 LEARN_RATE = 1e-5
 
-# Delete all content in tmp/npy/
-filelist = [f for f in os.listdir('tmp/npy')]
-for f in filelist:
-    os.remove(os.path.join('tmp/npy', f))
-
-# Access Google Cloud Storage
-gcs_client = storage.Client.from_service_account_json(
-    '/home/harold_triedman/elvo-analysis/credentials/client_secret.json'
-)
-bucket = gcs_client.get_bucket('elvos')
-
-# Get label data from Google Cloud Storage
-blob = storage.Blob('augmented_annotated_labels.csv', bucket)
-blob.download_to_filename('tmp/augmented_annotated_labels.csv')
-prelim_label_data = {}
-with open('tmp/augmented_annotated_labels.csv', 'r') as pos_file:
-    reader = csv.reader(pos_file, delimiter=',')
-    for row in reader:
-        if row[1] != 'Unnamed: 0':
-            prelim_label_data[row[1]] = int(row[2])
-
-# Get all of the positives from the label data
-positive_label_data = {}
-print('getting 12168 positive labels')
-for id_, label in list(prelim_label_data.items()):
-    if label == 1 and '_' in id_:
-        positive_label_data[id_] = label
-
-# Get 14500 random negatives from the label data to feed into our generator
-negative_counter = 0
-negative_label_data = {}
-print("getting 14500 random negative labels")
-while negative_counter < 14500:
-    id_, label = random.choice(list(prelim_label_data.items()))
-    if label == 0:
-        negative_label_data[id_] = label
-        del prelim_label_data[id_]
-        negative_counter += 1
-
-chunks = []
-labels = []
-
-i = 1
-for id_, label in list(negative_label_data.items()):
-    if i % 500 == 0:
-        print(f'got chunk {i}')
-    i += 1
-    blob = bucket.get_blob('chunk_data/normal/negative/' + id_ + '.npy')
-    arr = cloud_management.download_array(blob)
-    if arr.shape == (32, 32, 32):
-        arr = np.expand_dims(arr, axis=-1)
-        chunks.append(arr)
-        labels.append(label)
-print(f'{i} total negative chunks')
-
-i = 1
-for id_, label in list(positive_label_data.items()):
-    if i % 500 == 0:
-        print(f'got chunk {i}')
-    i += 1
-    blob = bucket.get_blob('chunk_data/normal/positive/' + id_ + '.npy')
-    arr = cloud_management.download_array(blob)
-    if arr.shape == (32, 32, 32):
-        arr = np.expand_dims(arr, axis=-1)
-        chunks.append(arr)
-        labels.append(label)
-print(f'{i} total positive chunks')
-
-tmp = list(zip(chunks, labels))
-random.shuffle(tmp)
-chunks, labels = zip(*tmp)
-
-# Split based on validation
-print('splitting based on validation split')
-full_x_train = np.asarray(chunks[int(len(chunks) * 0.1):])
-full_y_train = np.asarray(labels[int(len(labels) * 0.1):])
-
-full_x_val = np.asarray(chunks[:int(len(chunks) * 0.1)])
-full_y_val = np.asarray(labels[:int(len(labels) * 0.1)])
-
-print(f'{len(chunks)} total chunks to train with')
-
-print(f'full training data: {full_x_train.shape}, {full_y_train.shape}')
-print(f'full validation data: {full_x_val.shape}, {full_y_val.shape}')
-
 sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
+with open('chunk_data.pkl', 'wb') as infile:
+    full_data = pickle.load(infile)
+
+full_x_train = full_data[0]
+full_y_train = full_data[1]
+full_x_val = full_data[2]
+full_y_val = full_data[3]
 
 metrics = ['acc',
            utils.true_positives,
