7ab7732f4cb32fa64abc6da9452bc2db1c746902
Hal Triedman <harold_triedman@brown.edu>
2018-07-18 17:39:01 -0400

    fixed prints

--- etl/roi_train_preprocess.py
+++ etl/roi_train_preprocess.py
@@ -1,10 +1,10 @@
-import os
 import csv
 import numpy as np
 import random
 from google.cloud import storage
 from lib import cloud_management
 import pickle
+import logging
 
 # Access Google Cloud Storage
 gcs_client = storage.Client.from_service_account_json(
@@ -24,7 +24,7 @@ with open('tmp/augmented_annotated_labels.csv', 'r') as pos_file:
 
 # Get all of the positives from the label data
 positive_label_data = {}
-print('getting 12168 positive labels')
+logging.info('getting 12168 positive labels')
 for id_, label in list(prelim_label_data.items()):
     if label == 1 and '_' in id_:
         positive_label_data[id_] = label
@@ -32,12 +32,12 @@ for id_, label in list(prelim_label_data.items()):
 # Get 14500 random negatives from the label data to feed into our generator
 negative_counter = 0
 negative_label_data = {}
-print("getting 14500 random negative labels")
+logging.info("getting 14500 random negative labels")
 while negative_counter < 14500:
     id_, label = random.choice(list(prelim_label_data.items()))
     if label == 0:
         if negative_counter % 500 == 0:
-            print(f'gotten {negative_counter} labels so far')
+            logging.info(f'gotten {negative_counter} labels so far')
         negative_label_data[id_] = label
         del prelim_label_data[id_]
         negative_counter += 1
@@ -48,7 +48,7 @@ labels = []
 i = 1
 for id_, label in list(negative_label_data.items()):
     if i % 500 == 0:
-        print(f'got chunk {i}')
+        logging.info(f'got chunk {i}')
     i += 1
     blob = bucket.get_blob('chunk_data/normal/negative/' + id_ + '.npy')
     arr = cloud_management.download_array(blob)
@@ -56,12 +56,12 @@ for id_, label in list(negative_label_data.items()):
         arr = np.expand_dims(arr, axis=-1)
         chunks.append(arr)
         labels.append(label)
-print(f'{i} total negative chunks')
+logging.info(f'{i} total negative chunks')
 
 i = 1
 for id_, label in list(positive_label_data.items()):
     if i % 500 == 0:
-        print(f'got chunk {i}')
+        logging.info(f'got chunk {i}')
     i += 1
     blob = bucket.get_blob('chunk_data/normal/positive/' + id_ + '.npy')
     arr = cloud_management.download_array(blob)
@@ -69,24 +69,24 @@ for id_, label in list(positive_label_data.items()):
         arr = np.expand_dims(arr, axis=-1)
         chunks.append(arr)
         labels.append(label)
-print(f'{i} total positive chunks')
+logging.info(f'{i} total positive chunks')
 
 tmp = list(zip(chunks, labels))
 random.shuffle(tmp)
 chunks, labels = zip(*tmp)
 
 # Split based on validation
-print('splitting based on validation split')
+logging.info('splitting based on validation split')
 full_x_train = np.asarray(chunks[int(len(chunks) * 0.1):])
 full_y_train = np.asarray(labels[int(len(labels) * 0.1):])
 
 full_x_val = np.asarray(chunks[:int(len(chunks) * 0.1)])
 full_y_val = np.asarray(labels[:int(len(labels) * 0.1)])
 
-print(f'{len(chunks)} total chunks to train with')
+logging.info(f'{len(chunks)} total chunks to train with')
 
-print(f'full training data: {full_x_train.shape}, {full_y_train.shape}')
-print(f'full validation data: {full_x_val.shape}, {full_y_val.shape}')
+logging.info(f'full training data: {full_x_train.shape}, {full_y_train.shape}')
+logging.info(f'full validation data: {full_x_val.shape}, {full_y_val.shape}')
 
 full_arr = np.array([full_x_train,
                      full_y_train,
