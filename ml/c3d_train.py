7c8e2b58e1da0a7fd9a77011dd75af119302d9c3
Hal Triedman <harold_triedman@brown.edu>
2018-07-18 16:21:20 -0400

    get roi-preprocessing branch up to date

---
+++ ml/c3d_train.py
@@ -0,0 +1,153 @@
+import os
+import tensorflow as tf
+from ml.models.three_d import c3d
+import os
+import csv
+import numpy as np
+import random
+from google.cloud import storage
+from etl.lib import cloud_management
+
+from keras.optimizers import SGD
+from keras import metrics
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint
+
+BLACKLIST = []
+LEARN_RATE = 1e-5
+
+
+def true_positives(y_true, y_pred):
+    return K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+
+
+def false_negatives(y_true, y_pred):
+    return K.sum(K.round(K.clip(y_true * (1 - y_pred), 0, 1)))
+
+
+def sensitivity(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    return true_positives / (possible_positives + K.epsilon())
+
+
+def specificity(y_true, y_pred):
+    true_negatives = K.sum(K.round(K.clip((1 - y_true) * (1 - y_pred), 0, 1)))
+    possible_negatives = K.sum(K.round(K.clip(1 - y_true, 0, 1)))
+    return true_negatives / (possible_negatives + K.epsilon())
+
+
+# Delete all content in tmp/npy/
+filelist = [f for f in os.listdir('tmp/npy')]
+for f in filelist:
+    os.remove(os.path.join('tmp/npy', f))
+
+# Access Google Cloud Storage
+gcs_client = storage.Client.from_service_account_json(
+    '/home/harold_triedman/elvo-analysis/credentials/client_secret.json'
+)
+bucket = gcs_client.get_bucket('elvos')
+
+# Get label data from Google Cloud Storage
+blob = storage.Blob('augmented_annotated_labels.csv', bucket)
+blob.download_to_filename('tmp/augmented_annotated_labels.csv')
+prelim_label_data = {}
+with open('tmp/augmented_annotated_labels.csv', 'r') as pos_file:
+    reader = csv.reader(pos_file, delimiter=',')
+    for row in reader:
+        if row[1] != 'Unnamed: 0':
+            prelim_label_data[row[1]] = int(row[2])
+
+# Get all of the positives from the label data
+positive_label_data = {}
+print('getting 12168 positive labels')
+for id_, label in list(prelim_label_data.items()):
+    if label == 1 and '_' in id_:
+        positive_label_data[id_] = label
+
+# Get 14500 random negatives from the label data to feed into our generator
+negative_counter = 0
+negative_label_data = {}
+print("getting 14500 random negative labels")
+while negative_counter < 14500:
+    id_, label = random.choice(list(prelim_label_data.items()))
+    if label == 0:
+        negative_label_data[id_] = label
+        del prelim_label_data[id_]
+        negative_counter += 1
+
+chunks = []
+labels = []
+
+i = 1
+for id_, label in list(negative_label_data.items()):
+    if i % 500 == 0:
+        print(f'got chunk {i}')
+    i += 1
+    blob = bucket.get_blob('chunk_data/normal/negative/' + id_ + '.npy')
+    arr = cloud_management.download_array(blob)
+    if arr.shape == (32, 32, 32):
+        arr = np.expand_dims(arr, axis=-1)
+        chunks.append(arr)
+        labels.append(label)
+print(f'{i} total negative chunks')
+
+i = 1
+for id_, label in list(positive_label_data.items()):
+    if i % 500 == 0:
+        print(f'got chunk {i}')
+    i += 1
+    blob = bucket.get_blob('chunk_data/normal/positive/' + id_ + '.npy')
+    arr = cloud_management.download_array(blob)
+    if arr.shape == (32, 32, 32):
+        arr = np.expand_dims(arr, axis=-1)
+        chunks.append(arr)
+        labels.append(label)
+print(f'{i} total positive chunks')
+
+tmp = list(zip(chunks, labels))
+random.shuffle(tmp)
+chunks, labels = zip(*tmp)
+
+# Split based on validation
+print('splitting based on validation split')
+X_train = np.asarray(chunks[int(len(chunks) * 0.1):])
+y_train = np.asarray(labels[int(len(labels) * 0.1):])
+
+X_val = np.asarray(chunks[:int(len(chunks) * 0.1)])
+y_val = np.asarray(labels[:int(len(labels) * 0.1)])
+
+print(f'{len(chunks)} total chunks to train with')
+
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+
+metrics.true_positives = true_positives
+metrics.false_negtives = false_negatives
+metrics.sensitivity = sensitivity
+metrics.specificity = specificity
+
+model = c3d.C3DBuilder.build()
+model.compile(optimizer=SGD(lr=LEARN_RATE, momentum=0.9, nesterov=True),
+              loss={"out_class": "binary_crossentropy"},
+#               loss='binary_crossentropy'
+              metrics={"out_class": [metrics.binary_accuracy,
+                                     metrics.binary_crossentropy,
+                                     true_positives,
+                                     false_negatives,
+                                     sensitivity,
+                                     specificity]
+                      }
+             )
+
+print(X_train.shape, y_train.shape)
+print(X_val.shape, y_val.shape)
+
+mc_callback = ModelCheckpoint(filepath='tmp/c3d_weights.hdf5', verbose=1)
+model.fit(
+    x=X_train,
+    y=y_train,
+    epochs=100,
+    batch_size=16,
+    callbacks=[mc_callback],
+    validation_data = (X_val, y_val),
+    verbose=1)
