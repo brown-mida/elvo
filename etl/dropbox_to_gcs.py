be129e71b5366af09cea5ce9e351876d1d3d12d6
luke-zhu <luke_zhu@brown.edu>
2018-07-19 00:20:10 -0400

    Add functionality to load data from dropbox 7_17 New ELVOs

--- etl/dropbox_to_gcs.py
+++ etl/dropbox_to_gcs.py
@@ -1,8 +1,10 @@
 """Moves new data in https://www.dropbox.com/home/ELVOs_anon
 to gs://elvos/elvos_anon.
 
-To run this code type the following commands in your terminal:
-    conda install -c conda-forge dropbox
+This script will only load top level files and files in
+specified subdirectories. The subdirectory files will
+be saved in the same GCS folder, meaning this script effectively
+flattens those files.
 
 Then set your google application credentials as explained here:
 https://cloud.google.com/docs/authentication/getting-started
@@ -11,9 +13,9 @@ Similarly set the environment variable DROPBOX_TOKEN to an
 access token.
 """
 import datetime
+import os
 
 import dropbox
-import os
 from dropbox.files import FileMetadata, FolderMetadata
 from google.cloud import storage
 
@@ -23,16 +25,30 @@ def upload_entry_if_outdated(entry: FileMetadata,
                              bucket: storage.Bucket):
     """Uploads the entry to Google Cloud"""
     blob = storage.Blob('ELVOs_anon/' + entry.name, bucket)
-    blob.reload()
-    dropbox_time = entry.server_modified.replace(tzinfo=datetime.timezone.utc)
-    if dropbox_time < blob.updated:
-        print('blob {} is not outdated, not uploading'.format(blob.name))
+
+    if blob.exists():
+        blob.reload()
+        dropbox_time = entry.server_modified.replace(
+            tzinfo=datetime.timezone.utc)
     else:
-        print('downloading from dropbox:', entry.name)
-        dbx.files_download_to_file(entry.name, entry.id)
-        print('uploading to GCS:', 'ELVOs_anon/' + entry.name)
-        blob.upload_from_filename(entry.name)
-        os.remove(entry.name)
+        dropbox_time = None
+
+    if not dropbox_time:
+        print('blob {} does not exist, inserting'.format(blob.name))
+        upload_entry(blob, dbx, entry)
+    elif dropbox_time >= blob.updated:
+        print('blob {} is outdated, updating'.format(blob.name))
+        upload_entry(blob, dbx, entry)
+    else:
+        print('blob {} is not outdated, not uploading'.format(blob.name))
+
+
+def upload_entry(blob, dbx, entry):
+    print('downloading from dropbox:', entry.name)
+    dbx.files_download_to_file(entry.name, entry.id)
+    print('uploading to GCS:', 'ELVOs_anon/' + entry.name)
+    blob.upload_from_filename(entry.name)
+    os.remove(entry.name)
 
 
 if __name__ == '__main__':
@@ -47,7 +63,13 @@ if __name__ == '__main__':
         raise RuntimeError('has_more=True is currently not supported')
 
     for entry in results.entries:
-        if isinstance(entry, FolderMetadata):
-            print('ignoring folder:', entry)
+        # TODO(#102): Explain '7_11 Redownloaded Studies'
+        if entry.name in ('7_17 New ELVOs'):
+            print('uploading files in folder:', entry.name)
+            subdir_results = dbx.files_list_folder(entry.id)
+            for e in subdir_results.entries:
+                upload_entry_if_outdated(e, dbx, bucket)
+        elif isinstance(entry, FolderMetadata):
+            print('ignoring folder:', entry.name)
         else:
             upload_entry_if_outdated(entry, dbx, bucket)
