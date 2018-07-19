970e7ef36517c6bbe4e9198a71c5bda020d89d01
rladbsgh <therladbsgh@gmail.com>
2018-07-19 17:27:47 -0400

    Fix edge case for zoom_range v2

--- ml/blueno_validate.py
+++ ml/blueno_validate.py
@@ -118,7 +118,8 @@ def get_models_to_train(address, lower, upper, data_dir):
             else:
                 generator[param] = default_value
         # Edge case for zooms
-        if generator['zoom_range'][0] == '(':
+        if (isinstance(generator['zoom_range'], str) and
+           generator['zoom_range'][0] == '('):
             generator['zoom_range'] = generator['zoom_range'][1:-1].split(',')
 
         generator['generator_callable'] = standard_generators
