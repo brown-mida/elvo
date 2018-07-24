28492595b92503096936850b9da911783b9e44f7
Hal Triedman <harold_triedman@brown.edu>
2018-07-24 15:29:59 -0400

    cleaned up one hot encoding labeling for categorization

--- etl/roi_preprocess_classification.py
+++ etl/roi_preprocess_classification.py
@@ -1,10 +1,6 @@
 import logging
-
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
-
-from lib import cloud_management as cloud  # , roi_transforms, transforms
 
 
 def configure_logger():
@@ -19,68 +15,85 @@ def configure_logger():
 
 def process_labels():
     positives_df = pd.read_csv(
-        '/Users/amy/Documents/ELVOproject/ELVO_Annotator_Key_Positives.csv')
-    # print(positives_df['0', '1', '2', '3', '4'])
+        'ELVO_Annotator_Key_Positives.csv')
+
     positives_df.drop([0, 1, 2])
     positives_df.drop(positives_df.columns[0], axis=1)
-    positives_df = positives_df.filter(items=['Unnamed: 0',
-                                                  'Unnamed: 3'])
-    for index, row in positives_df.iterrows():
-        print(index, row)
-
+    positives_df = positives_df.filter(items=['Anon ID',
+                                              'Location of occlusion/s'])
     logging.info(positives_df)
-
     negatives_df = pd.read_csv(
-        '/Users/amy/Documents/ELVOproject/ELVO_Annotator_Key_Negatives.csv')
+        'ELVO_Annotator_Key_Negatives.csv'
+    )
     logging.info(negatives_df)
-
     to_add = {}
     for index, row in negatives_df.iterrows():
         patient_id = row[0]
-        #(m1, m2, m3, ica, basilar, vert)
-        to_add[patient_id] = str((0, 0, 0, 0, 0, 0))
+        to_add[patient_id] = np.zeros(7)
 
+    """
+    Key for one-hot encoding types of strokes:
+    idx 0: L MCA
+    idx 1: R MCA
+    idx 2: L ICA
+    idx 3: R ICA
+    idx 4: L Vertebral
+    idx 5: R Vertebral
+    idx 6: Basilar
+    """
     for index, row in positives_df.iterrows():
         patient_id = row[0]
-        m1 = 0
-        m2 = 0
-        m3 = 0
-        ica = 0
-        basilar = 0
-        vert = 0
-        print("unnamed 0: " + str(row['Unnamed: 0'])
-                + "unnamed 3: " + str(row['Unnamed: 3']))
-        if '**' in str(row['Unnamed: 0']) \
-                or 'Anon' in str(row['Unnamed: 0']) \
-                or row['Unnamed: 0'] == '':
-            continue
-        if 'M1' in str(row['Unnamed: 3']):
-            m1 = 1
-        if 'M2' in str(row['Unnamed: 3']):
-            m2 = 1
-        if 'M3' in str(row['Unnamed: 3']):
-            m3 = 1
-        if 'ICA' in str(row['Unnamed: 3']):
-            ica = 1
-        # use 'asilar' to account for capitalization
-        if 'asilar' in str(row['Unnamed: 3']):
-            basilar = 1
-        if 'vert' in str(row['Unnamed: 3']):
-            vert = 1
-        to_add[patient_id] = str((m1, m2, m3, ica, basilar, vert))
+        occlusion_type = row[1].lower()
+        occlusion_label = np.zeros(7)
+        print("ID: " + patient_id
+              + "\nOcclusion type: " + occlusion_type)
 
-    to_add_df = pd.DataFrame.from_dict(
-        to_add, orient='index', columns=['Unnamed: 3'])
+        # L MCAs
+        if 'l m' in occlusion_type:
+            occlusion_label[0] = 1
+        # R MCAs
+        if 'r m' in occlusion_type:
+            occlusion_label[1] = 1
+        # L ICA
+        if 'l ica' in occlusion_type:
+            occlusion_label[2] = 1
+        # R ICA
+        if 'r ica' in occlusion_type:
+            occlusion_label[3] = 1
+        # L Vertebral
+        if 'l vert' in occlusion_type:
+            occlusion_label[4] = 1
+        # R Vertebral
+        if 'r vert' in occlusion_type:
+            occlusion_label[1] = 1
+        # Basilar
+        if 'basilar' in occlusion_type:
+            occlusion_label[1] = 1
+
+        to_add[patient_id] = occlusion_label
+
+    to_add_df = pd.DataFrame.from_dict(to_add,
+                                       orient='index',
+                                       columns=['L MCA',
+                                                'R MCA',
+                                                'L ICA',
+                                                'R ICA',
+                                                'L Vert',
+                                                'R Vert',
+                                                'Basilar']
+                                       )
     logging.info(to_add_df)
 
     to_add_df.to_csv(
-        '/Users/amy/Documents/ELVOproject/classification_vectors.csv')
+        'classification_vectors.csv'
+    )
     print("Saving file")
 
 
 def run_preprocess():
     configure_logger()
     process_labels()
+
 
 if __name__ == '__main__':
     run_preprocess()
