4e9e6f15f736bcc233a20ae6d382413fb9ad945d
amy <amyw98@comcast.net>
2018-07-24 14:12:50 -0400

    classification annotations r working (sort of)

--- etl/roi_preprocess_classification.py
+++ etl/roi_preprocess_classification.py
@@ -18,79 +18,48 @@ def configure_logger():
 
 
 def process_labels():
-    annotations_df = pd.read_csv(
-        '/home/amy/annotations.csv')
-    # annotations_df = pd.read_csv(
-    #         '/Users/haltriedman/Desktop/annotations.csv')
-    annotations_df = annotations_df.drop(['created_by',
-                                          'created_at',
-                                          'ROI Link',
-                                          'Unnamed: 10',
-                                          'Mark here if Matt should review'],
-                                         axis=1)
-    annotations_df = annotations_df[
-        annotations_df.red1 == annotations_df.red1]
-    logging.info(annotations_df)
-    return annotations_df
+    positives_df = pd.read_csv(
+        '/Users/amy/Documents/ELVOproject/ELVO_Annotator_Key_Positives.csv')
+    # print(positives_df['0', '1', '2', '3', '4'])
+    positives_df.drop([0, 1, 2])
+    positives_df.drop(positives_df.columns[0], axis=1)
+    positives_df = positives_df.filter(items=['Unnamed: 0',
+                                                  'Unnamed: 3'])
+    for index, row in positives_df.iterrows():
+        print(index, row)
 
+    logging.info(positives_df)
 
-def inspect_rois(annotations_df):
-    client = cloud.authenticate()
-    bucket = client.get_bucket('elvos')
+    negatives_df = pd.read_csv(
+        '/Users/amy/Documents/ELVOproject/ELVO_Annotator_Key_Negatives.csv')
+    logging.info(negatives_df)
 
-    # loop through every array on GCS
-    for in_blob in bucket.list_blobs(prefix='airflow/npy'):
-        # if in_blob.name != 'airflow/npy/ZZX0ZNWG6Q9I18GK.npy':
-        #     continue
-        # blacklist
-        if in_blob.name == 'airflow/npy/LAUIHISOEZIM5ILF.npy':
+    to_add = {}
+    for index, row in negatives_df.iterrows():
+        patient_id = row[0]
+        to_add[patient_id] = 'negative'
+
+    for index, row in positives_df.iterrows():
+        patient_id = row[0]
+        print("unnamed 0: " + str(row['Unnamed: 0'])
+                + "unnamed 3: " + str(row['Unnamed: 3']))
+        if '**' in str(row['Unnamed: 0']) \
+                or 'Anon' in str(row['Unnamed: 0']) \
+                or row['Unnamed: 0'] == '':
             continue
+        to_add[patient_id] = row['Unnamed: 3']
 
-        # get the file id
-        file_id = in_blob.name.split('/')[2]
-        file_id = file_id.split('.')[0]
+    to_add_df = pd.DataFrame.from_dict(
+        to_add, orient='index', columns=['Unnamed: 3'])
+    logging.info(to_add_df)
 
-        logging.info(f'chunking {file_id}')
-        # copy ROI if there's a positive match in the ROI annotations
-        roi_df = annotations_df[
-            annotations_df['patient_id'].str.match(file_id)]
-        # if it's empty, this brain is ELVO negative
-        if roi_df.empty:
-            elvo_positive = False
-        else:
-            elvo_positive = True
-
-        arr = cloud.download_array(in_blob)
-
-        # if it's elvo positive
-        if elvo_positive:
-            chunks = []
-            blue = int(len(arr) - roi_df['blue2'].iloc[0])
-            green = int(roi_df['green1'].iloc[0])
-            red = int(roi_df['red1'].iloc[0])
-            chunks.append(arr[blue: blue + 32,
-                          green: green + 50, red: red + 50])
-            chunks.append(arr[
-                          blue: blue + 32, red: red + 50, green: green + 50])
-            start = 0
-            for chunk in chunks:
-                logging.info(start)
-                axial = np.max(chunk, axis=0)
-                coronal = np.max(chunk, axis=1)
-                sag = np.max(chunk, axis=2)
-                fig, ax = plt.subplots(1, 3, figsize=(6, 4))
-                ax[0].imshow(axial, interpolation='none')
-                ax[1].imshow(coronal, interpolation='none')
-                ax[2].imshow(sag, interpolation='none')
-                plt.show()
-                start += 10
+    to_add_df.to_csv('/Users/amy/Documents/ELVOproject/classification_annotations.csv')
+    print("Saving file")
 
 
 def run_preprocess():
     configure_logger()
-    annotations_df = process_labels()
-    create_labels(annotations_df)
-
+    process_labels()
 
 if __name__ == '__main__':
     run_preprocess()
