8415886185cd885470a6271adef7389e4a785122
amy <amyw98@comcast.net>
2018-07-24 14:39:09 -0400

    made it vectors

--- etl/roi_preprocess_classification.py
+++ etl/roi_preprocess_classification.py
@@ -37,23 +37,44 @@ def process_labels():
     to_add = {}
     for index, row in negatives_df.iterrows():
         patient_id = row[0]
-        to_add[patient_id] = 'negative'
+        #(m1, m2, m3, ica, basilar, vert)
+        to_add[patient_id] = str((0, 0, 0, 0, 0, 0))
 
     for index, row in positives_df.iterrows():
         patient_id = row[0]
+        m1 = 0
+        m2 = 0
+        m3 = 0
+        ica = 0
+        basilar = 0
+        vert = 0
         print("unnamed 0: " + str(row['Unnamed: 0'])
                 + "unnamed 3: " + str(row['Unnamed: 3']))
         if '**' in str(row['Unnamed: 0']) \
                 or 'Anon' in str(row['Unnamed: 0']) \
                 or row['Unnamed: 0'] == '':
             continue
-        to_add[patient_id] = row['Unnamed: 3']
+        if 'M1' in str(row['Unnamed: 3']):
+            m1 = 1
+        if 'M2' in str(row['Unnamed: 3']):
+            m2 = 1
+        if 'M3' in str(row['Unnamed: 3']):
+            m3 = 1
+        if 'ICA' in str(row['Unnamed: 3']):
+            ica = 1
+        # use 'asilar' to account for capitalization
+        if 'asilar' in str(row['Unnamed: 3']):
+            basilar = 1
+        if 'vert' in str(row['Unnamed: 3']):
+            vert = 1
+        to_add[patient_id] = str((m1, m2, m3, ica, basilar, vert))
 
     to_add_df = pd.DataFrame.from_dict(
         to_add, orient='index', columns=['Unnamed: 3'])
     logging.info(to_add_df)
 
-    to_add_df.to_csv('/Users/amy/Documents/ELVOproject/classification_annotations.csv')
+    to_add_df.to_csv(
+        '/Users/amy/Documents/ELVOproject/classification_vectors.csv')
     print("Saving file")
 
 
