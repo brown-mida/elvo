e1fd622ce5ba5c446d686b16af02d91cb64f380c
luke-zhu <luke_zhu@brown.edu>
2018-07-24 16:12:07 -0400

    Create DAG to prepare numpy files for web app viewing

---
+++ etl/processed_dag.py
@@ -0,0 +1,89 @@
+import datetime
+import os
+
+import matplotlib
+import numpy as np
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from google.cloud import storage
+
+matplotlib.use('agg')
+
+from matplotlib import pyplot as plt
+
+# A list of subdirectories of 'gs://elvos/processed' to ignore
+BLACKLIST = [
+    'luke',
+    'luke1',
+    'mip_three10',
+    'mip_three4',
+]
+
+
+def to_public_png(npy_blob: storage.Blob, public_bucket: storage.Bucket):
+    """
+    Converts the .npy blob into a png file and uploads it to the public
+    bucket.
+
+    :param npy_blob:
+    :param public_bucket:
+    :return:
+    """
+    npy_filepath = f'/tmp/{npy_blob.name.split("/")[-1]}'
+    npy_blob.download_to_filename(npy_filepath)
+    arr = np.load(npy_filepath)
+
+    png_filepath = npy_filepath.replace('.npy', '.png')
+    plt.imsave(png_filepath, arr)
+
+    png_blob_name = npy_blob.name.replace('.npy', '.png')
+    png_blob = public_bucket.blob(png_blob_name)
+    png_blob.upload_from_filename(png_filepath)
+    os.remove(npy_filepath)
+    os.remove(png_filepath)
+
+
+def upload_numpy_files():
+    """
+    Uploads all numpy files to gs://elvos-public.
+
+    Only the extension of the name will be changed. For example
+    gs://elvos/processed/processed-lower/abc.npy will be uploaded
+    as gs://elvos-public/processed/processed-lower/abc.png
+
+    Blacklisted folders will be ignored.
+
+    :return:
+    """
+    gcs_client = storage.Client(project='elvo-198322')
+    in_bucket = gcs_client.get_bucket('elvos')
+    out_bucket = gcs_client.get_bucket('elvos-public')
+
+    blob: storage.Blob
+    for blob in in_bucket.list_blobs(prefix='processed/'):
+        to_ignore = False
+        for s in BLACKLIST:
+            if blob.name.split('/')[1] == s:
+                print(f'ignoring blacklisted blob {blob.name}')
+                to_ignore = True
+                break
+
+        if not to_ignore and blob.name.endswith('.npy'):
+            print(f'uploading blob {blob.name}')
+            to_public_png(blob, out_bucket)
+
+
+default_args = {
+    'owner': 'luke',
+    'email': 'luke_zhu@brown.edu',
+    'start_date': datetime.datetime(2018, 7, 24),
+}
+
+dag = DAG(dag_id='uploaded_processed_data',
+          description='Uploads processed numpy array data as pngs for the'
+                      ' web app',
+          default_args=default_args)
+
+upload_numpy_files_op = PythonOperator(task_id='upload_numpy_files',
+                                       python_callable=upload_numpy_files,
+                                       dag=dag)
