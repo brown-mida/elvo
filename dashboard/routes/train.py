d9ec6c0183e1e1368ec4b27e90f24c41a406cff5
luke-zhu <luke_zhu@brown.edu>
2018-07-25 18:53:10 -0400

    Add code to upload a copy of the data to gs://elvos/processed

--- dashboard/routes/train.py
+++ dashboard/routes/train.py
@@ -135,6 +135,22 @@ def preprocess_data(data_name: str):
 
 def _process_arrays(data_name, crop_length, height_offset,
                     mip_thickness, pixel_value_range):
+    """
+    Processes the arrays, loading PNGS to
+    gs://elvos-public/processed/{data_name}/arrays/ and npy files to
+    gs://elvos/processed/{data_name}/arrays
+
+    TODO(luke): Code to train on data only available on GCS (aka download
+    in bluenot.py).
+
+    :param data_name: the directory name of the data.
+    :param crop_length: the length (and width) of an image
+    :param height_offset: the offset from the top of the image
+        (above the head) to start mipping from.
+    :param mip_thickness: the thickness of one of the 3 mip slices
+    :param pixel_value_range: the range of values to bound the CT values at
+    :return:
+    """
     for input_blob in priv_bucket.list_blobs(prefix=f'airflow/npy'):
         logging.info(f'downloading numpy file: {input_blob.name}')
         input_filename = input_blob.name.split('/')[-1]
@@ -142,6 +158,7 @@ def _process_arrays(data_name, crop_length, height_offset,
         input_blob.download_to_file(input_stream)
         input_stream.seek(0)
         arr = np.load(input_stream)
+        input_stream.close()
 
         logging.debug(f'processing numpy array')
         try:
@@ -151,21 +168,30 @@ def _process_arrays(data_name, crop_length, height_offset,
             logging.debug(f'file {input_filename} could not be processed,'
                           f' has input shape {arr.shape}')
 
+        arr_stream = io.BytesIO()
+        np.save(arr_stream, arr)
+        arr_stream.seek(0)
+        arr_blob = priv_bucket.blob(
+            f'processed/{data_name}/arrays/{input_filename}')
+        logging.info(f'uploading npy file: {arr_blob.name}')
+        arr_blob.upload_from_file(arr_stream)
+
         logging.debug(f'converting processed array into png')
-        output_stream = io.BytesIO()
+        png_stream = io.BytesIO()
         # Standardize to [0, 1] otherwise it fails
         # TODO(luke): Consider standardizing by feature (RGB) as that is how
         # Keras does it
         standardized = (arr - arr.min()) / arr.max()
-        plt.imsave(output_stream, standardized)
-        output_stream.seek(0)
+        plt.imsave(png_stream, standardized)
+        png_stream.seek(0)
 
-        output_filename = input_filename.replace('.npy', '.png')
-        output_blob = pub_bucket.blob(
-            f'processed/{data_name}/arrays/{output_filename}')
+        png_filename = input_filename.replace('.npy', '.png')
+        png_blob = pub_bucket.blob(
+            f'processed/{data_name}/arrays/{png_filename}')
 
-        logging.info(f'uploading png file: {output_blob.name}')
-        output_blob.upload_from_file(output_stream)
+        logging.info(f'uploading png file: {png_blob.name}')
+        png_blob.upload_from_file(png_stream)
+        png_stream.close()
 
 
 def _process_arr(arr,
