4d41adac5d987781122b5ce5b200e2e7cf7b983a
luke-zhu <luke_zhu@brown.edu>
2018-07-30 17:02:36 -0400

    Delete the dashboard directory
    The project has been moved to https://github.com/elvoai/blueno-ui

--- dashboard/utils/mongodb.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-
-
-def get_db():
-    client = pymongo.MongoClient(
-        "mongodb://bluenoml:elvoanalysis@104.196.51.205/elvo"
-    )
-    datasets = client.elvo.datasets
-    return datasets
