98d825ee11ac07d859a86589afd34b6872e5431e
Hal Triedman <harold_triedman@brown.edu>
2018-08-02 11:09:12 -0400

    test chunk management script

--- etl/get_test_data.py
+++ etl/get_test_data.py
@@ -7,12 +7,12 @@ with open('chunk_data_separated_ids_hard.pkl', 'rb') as infile:
 
 x_test = np.asarray(full_data[4])
 y_test = np.asarray(full_data[5])
-print(x_test.shape, y_test.shape)
 
-test = np.array([x_test, y_test])
+with open('test_train_hard.pkl', 'wb') as outfile:
+    pickle.dump(x_test, outfile, pickle.HIGHEST_PROTOCOL)
 
-with open('test_data_hard.pkl', 'wb') as outfile:
-    pickle.dump(test, outfile, pickle.HIGHEST_PROTOCOL)
+with open('test_val_hard.pkl', 'wb') as outfile:
+    pickle.dump(y_test, outfile, pickle.HIGHEST_PROTOCOL)
 
 # load and unpack data separated by IDs (from etl/roi_train_preprocess.py)
 with open('chunk_data_separated_ids.pkl', 'rb') as infile:
@@ -20,9 +20,9 @@ with open('chunk_data_separated_ids.pkl', 'rb') as infile:
 
 x_test = np.asarray(full_data[4])
 y_test = np.asarray(full_data[5])
-print(x_test.shape, y_test.shape)
 
-test = np.array([x_test, y_test])
+with open('test_train.pkl', 'wb') as outfile:
+    pickle.dump(x_test, outfile, pickle.HIGHEST_PROTOCOL)
 
-with open('test_data.pkl', 'wb') as outfile:
-    pickle.dump(test, outfile, pickle.HIGHEST_PROTOCOL)
+with open('test_val.pkl', 'wb') as outfile:
+    pickle.dump(y_test, outfile, pickle.HIGHEST_PROTOCOL)
