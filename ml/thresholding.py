2451357006bc76060434e1aed2933591963b48d2
luke-zhu <luke_zhu@brown.edu>
2018-08-07 14:55:47 -0400

    Update the thresholding script

--- ml/thresholding.py
+++ ml/thresholding.py
@@ -1,18 +1,62 @@
-"""Script for computing a matrix of statistics at each
-threshold.
+"""One-off script for computing a matrix of "threshold" statistics.
+
+If you wish to use this script, alter the model path and training
+parameters at the bottom of the script.
+
+Author: luke
 """
+import os
 
 import keras
 import numpy as np
+import pandas as pd
 from keras.losses import categorical_crossentropy
-from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.metrics import classification, roc_auc_score
 
 import blueno
 from blueno.preprocessing import prepare_data
 from generators.luke import standard_generators
-from models.luke import resnet
+
+# So keras doesn't attempt to use the gpus
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+
+def load_training_xy(data_root):
+    # Minimal params to generate the correct training-validation data
+    # split
+    params = blueno.ParamConfig(
+        data=blueno.DataConfig(
+            data_dir='{}/arrays'.format(data_root),
+            labels_path='{}/labels.csv'.format(data_root),
+            index_col='Anon ID',
+            label_col='occlusion_exists',
+            gcs_url='gs://elvos/processed/processed-new-training-2'),
+        generator=blueno.GeneratorConfig(
+            generator_callable=standard_generators),
+        model=blueno.ModelConfig(model_callable=None,
+                                 optimizer=None,
+                                 loss=categorical_crossentropy,
+                                 dropout_rate1=None,
+                                 dropout_rate2=None),
+        batch_size=None,
+        seed=0,  # or 0
+        val_split=0.1,
+    )
+    arrays = prepare_data(params, train_test_val=False)
+    return arrays[0], arrays[2]
+
+
+def load_test_xy(arrays_dirpath: str, labels_filepath: str):
+    array_dict = blueno.io.load_arrays(arrays_dirpath)
+    index_col = 'Anon ID'
+    labels_df = pd.read_csv(labels_filepath,
+                            index_col=index_col)
+    label_col = 'occlusion_exists'
+    label_series = labels_df[label_col]
+    x, y, _ = blueno.preprocessing.to_arrays(array_dict, label_series,
+                                             sort=True)
+    return x, y
 
 
 def print_metrics(y_true, y_pred):
@@ -41,27 +85,21 @@ def print_metrics(y_true, y_pred):
 def compute_thresholds(model: keras.Model,
                        x: np.ndarray,
                        y_true: np.ndarray):
-    for i in range(0, 11):
-        y_pred = model.predict(x)
+    y_pred = model.predict(x)
+
+    for i in range(1, 10):
+        threshold = i / 10
 
         if y_pred.shape[-1] == 1:
-            threshold = i / 10
-
             y_pred_classes = (y_pred > threshold).astype('int32')
             y_true_classes = (y_true > threshold).astype('int32')
-            print('report for threshold of {}'.format(threshold))
         elif y_pred.shape[-1] == 2:
-            offset = i / 10 - 0.5
-            y_pred[:, 1] += offset
-            y_pred_classes = y_pred.argmax(axis=1)
-            y_true_classes = y_true.argmax(axis=1)
-            print('\n\nreport for offset of {}'.format(offset))
-            print(
-                "(where a prediction y=[0.15, 0.25] is argmax{0.15, 0.25 + "
-                "offset})")
+            y_pred_classes = (y_pred[:, 1] > threshold).astype('int32')
+            y_true_classes = (y_true[:, 1] > threshold).astype('int32')
         else:
             raise ValueError('Only binary models are supported')
 
+        print('\n\nreport for threshold of {}'.format(threshold))
         y_true_binary = y_true_classes > 0
         y_pred_binary = y_pred_classes > 0
         print_metrics(y_true_binary, y_pred_binary)
@@ -80,42 +118,30 @@ def test_compute_thresholds():
 
 
 if __name__ == '__main__':
-    # TODO: Put the right model here
-    model_path = '/home/lzhu7/elvo-analysis/models/' \
-                 'processed-lower_2-classes-2018-08-02T15:31:14.880339.hdf5'
+    # Put the model you want to load here
+
+    # gs://elvos/models/
+    # processed-lower_2-classes-2018-07-13T09:59:22.804773.hdf5 .
+    model_dir = '/home/lzhu7/elvo-analysis/models'
+    model_name = 'processed-lower_2-classes-2018-07-13T09:59:22.804773.hdf5'
+    model_path = '{}/{}'.format(model_dir, model_name)
     model = blueno.io.load_model(model_path, compile=False)
 
-    # TODO: Make sure to specify the correct data
-    params = blueno.ParamConfig(
-        data=blueno.DataConfig(
-            data_dir='/home/lzhu7/elvo-analysis/data/processed-lower/arrays',
-            labels_path='/home/lzhu7/elvo-analysis/data/processed-lower/labels'
-                        '.csv',
-            index_col='Anon ID',
-            label_col='occlusion_exists',
-            gcs_url='gs://elvos/processed/processed-lower'),
-        generator=blueno.GeneratorConfig(
-            generator_callable=standard_generators,
-            rotation_range=20,
-            width_shift_range=0.1,
-            height_shift_range=0.1,
-            shear_range=0,
-            zoom_range=0.1,
-            horizontal_flip=True,
-            vertical_flip=False),
-        model=blueno.ModelConfig(model_callable=resnet,
-                                 optimizer=Adam(lr=1e-5),
-                                 loss=categorical_crossentropy,
-                                 dropout_rate1=0.7,
-                                 dropout_rate2=0.7, freeze=False),
-        batch_size=5, seed=42, val_split=0.1, max_epochs=100,
-        early_stopping=True, reduce_lr=False,
-    )
+    data_root = '/home/lzhu7/elvo-analysis/data/processed-lower'
+    x_train, y_train = load_training_xy(data_root)
+    test_data_dir = '/home/lzhu7/elvo-analysis/data/' \
+                    'processed-lower-test-155-samples'
+    x_test, y_test = load_test_xy(test_data_dir + '/arrays',
+                                  test_data_dir + '/labels.csv')
 
-    arrays = prepare_data(params, train_test_val=False)
-    x_train, x_valid, y_train, y_valid, id_train, id_valid = arrays
-    valid_datagen = ImageDataGenerator(featurewise_center=True,
-                                       featurewise_std_normalization=True)
-    valid_datagen.fit(x_train)
-    x_valid = valid_datagen.standardize(x_valid.astype(np.float32))
-    compute_thresholds(model, x_valid, y_valid)
+    datagen = ImageDataGenerator(featurewise_center=True,
+                                 featurewise_std_normalization=True)
+    datagen.fit(x_train)
+    x_train = datagen.standardize(x_train.astype(np.float32))
+    x_test = datagen.standardize(x_test.astype(np.float32))
+    y_test = keras.utils.to_categorical(y_test)
+
+    print(y_train)
+
+    compute_thresholds(model, x_train, y_train)
+    compute_thresholds(model, x_test, y_test)
