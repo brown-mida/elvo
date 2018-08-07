5f4225647c01ceb767e4ac10ed7d61c35db6d563
amy <amyw98@comcast.net>
2018-08-07 11:04:54 -0400

    test

--- etl/mip_dag.py
+++ etl/mip_dag.py
@@ -14,13 +14,25 @@ from mip import normal_mip
 from multichannel_mip import multichannel_mip
 from overlap_mip import overlap_mip
 
+# Set outermost parameters
 default_args = {
     'owner': 'hal',
     'start_date': datetime(2018, 6, 28, 5),
 }
 
+# Create DAG
+# dag_id: DAG name, self explanatory
+# description: description of DAG's purpose
+# default_args: previously specified params
+# catchup: whether or not intervals are automated; set to False to
+#         indicate that the DAG will only run for the most current instance
+#         of the interval series
+# max_active_runs: maximum number of active DAG runs, beyond this
+#         number of DAG runs in a running state, the scheduler won't create
+#         new active DAG runs
 dag = DAG(dag_id='mip_dag', default_args=default_args)
 
+# Define operations:
 axial_to_coronal_and_sagittal_op = \
     PythonOperator(task_id='axial_to_coronal_and_sagittal',
                    python_callable=axial_to_coronal_and_sagittal,
@@ -38,6 +50,13 @@ overlap_mip_op = PythonOperator(task_id='overlap_mip',
                                 python_callable=overlap_mip,
                                 dag=dag)
 
+# Define operation in DAG (notify Slack channel with a token)
+# task_id: name of operation, self explanatory
+# channel: the Slack channel that will display the notification
+# username: the Slack user who will be listed as posting the notification
+# token: the Slack token
+# text: text of Slack token
+# dag: the previously created DAG
 slack_confirmation = SlackAPIPostOperator(
     task_id='slack_confirmation',
     channel='i-utra',
@@ -47,5 +66,6 @@ slack_confirmation = SlackAPIPostOperator(
     dag=dag,
 )
 
+# Specify order of DAG operations
 axial_to_coronal_and_sagittal_op >> normal_mip_op >> \
     multichannel_mip_op >> overlap_mip_op >> slack_confirmation
