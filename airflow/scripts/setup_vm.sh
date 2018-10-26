3db9294eff5a1e2e8e327d123451fd0b5a22e91b
luke-zhu <luke_zhu@brown.edu>
2018-10-26 17:47:14 -0400

    Update requirements and setup script

--- airflow/scripts/setup_vm.sh
+++ airflow/scripts/setup_vm.sh
@@ -1,13 +1,22 @@
 # You should run this only once, when first setting up airflow
-# on a new VM. The VM should have Python 3.6 installed.
+# on a new VM. This works on Ubuntu 18.04 LTS with only the repo cloned
 #!/usr/bin/env bash
 
-export AIRFLOW_HOME=$PWD
-echo export AIRFLOW_HOME=$PWD >> ~/.bashrc # So $AIRFLOW_HOME is permanently set
+# Install Unix packages
+sudo apt-get update
+sudo apt-get install python3-pip python3-venv -y
 
+# So $AIRFLOW_HOME is permanently set in the shell
+echo export AIRFLOW_HOME=$PWD >> ~/.bashrc
+export AIRFLOW_HOME=$PWD
+
+# Set up a virtual environment
 python3 -m venv venv
 source $PWD/venv/bin/activate
 
+# Install all requirements
+export AIRFLOW_GPL_UNIDECODE=yes # So we can install airflow
 pip install -r requirements.txt
 
+# Initialize airflow
 airflow initdb
