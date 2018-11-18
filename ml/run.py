b53c7fad0d4bf3a477d5a13a2ef43b6c3e054a3d
mdong5 <mdong5@cs.brown.edu>
2018-11-18 13:17:56 -0500

    create new branch for multiphase pipeline training on each phase separately

--- ml/run.py
+++ ml/run.py
@@ -8,26 +8,26 @@ import generators.luke
 import models.luke
 
 USER = 'mary'
-BLUENO_HOME = '/home/lzhu7/elvo-analysis/'
-DATA_DIR = f'{BLUENO_HOME}data/'
+BLUENO_HOME = '/research/rih-cs/datasets/elvo/v1/'
+DATA_DIR = f'{BLUENO_HOME}preprocessed/'
 LOG_DIR = f'{BLUENO_HOME}logs/'
-
-NUM_GPUS = 1
-GPU_OFFSET = 2
-
 SLACK_TOKEN = 'xoxp-314216549302-331430419907-396979178437-' \
               'ae769a026a3c0f91623e9a6565f0d9ee'
 
+NUM_GPUS = 2
+GPU_OFFSET = 2
+
+# a lot of .npy files in data/ and preprocessed/ and labels.csv (patient_id, label 0 or 1)
+
 model_list = list(model_selection.ParameterGrid({
     'model_callable': [models.luke.resnet],
-    'dropout_rate1': [0.7],
-    'dropout_rate2': [0.7],
+    'dropout_rate1': [0.8],
+    'dropout_rate2': [0.8],
     'optimizer': [
         keras.optimizers.Adam(lr=1e-5),
     ],
     'loss': [
         keras.losses.categorical_crossentropy,
-        keras.losses.binary_crossentropy,
     ],
     'freeze': [False],
 }))
@@ -37,20 +37,20 @@ model_list = [blueno.ModelConfig(**m) for m in model_list]
 PARAM_GRID = model_selection.ParameterGrid({
     'data': [blueno.DataConfig(
         data_dir=str(pathlib.Path(DATA_DIR) /
-                     'processed-lower/arrays/'),
+                     'processed-new-training-2/arrays/'),
         labels_path=str(
-            pathlib.Path(DATA_DIR) / 'processed-lower/labels.csv'),
+            pathlib.Path(DATA_DIR) / 'processed-new-training-2/labels.csv'),
         index_col='Anon ID',
         label_col='occlusion_exists',
-        gcs_url='gs://elvos/processed/processed-lower')
+        gcs_url='gs://elvos/processed/processed-new-training-2')
     ],
     'generator': [blueno.GeneratorConfig(
         generator_callable=generators.luke.standard_generators,
         rotation_range=30)],
     'model': model_list,
-    'batch_size': [8],
-    'seed': [42, 0],
-    'val_split': [0.2, 0.1],
+    'batch_size': [5],
+    'seed': [0],
+    'val_split': [0.1],  # So we run the grid 16 times
 })
 
 PARAM_GRID = [blueno.ParamConfig(**p) for p in PARAM_GRID]
